2d55f483283d1470bef283a4f8796ed2d9a51422
James Munnelly <james.munnelly@jetstack.io>
2017-07-22 02:54:06 +0100

    Implement basic certificate issuer

--- pkg/controller/certificates/sync.go
+++ pkg/controller/certificates/sync.go
@@ -1,7 +1,14 @@
 package certificates
 
 import (
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
+	"sort"
+
+	api "k8s.io/api/core/v1"
+	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/jetstack/cert-manager/pkg/apis/certmanager/v1alpha1"
 	"github.com/jetstack/cert-manager/pkg/controller"
@@ -32,57 +39,58 @@ func sync(ctx *controller.Context, crt *v1alpha1.Certificate) error {
 		return err
 	}
 
-	// // step one: check if referenced secret exists, if not, trigger issue event
-	// secret, err := ctx.InformerFactory.Core().V1().Secrets().Lister().Secrets(crt.Namespace).Get(crt.Spec.SecretName)
+	// step one: check if referenced secret exists, if not, trigger issue event
+	secret, err := ctx.InformerFactory.Core().V1().Secrets().Lister().Secrets(crt.Namespace).Get(crt.Spec.SecretName)
 
-	// if err != nil {
-	// 	// TODO (@munnerz): only issue a certificate if the call failed due to
-	// 	// no resource being found
-	// 	return c.issue(crt)
-	// }
+	if err != nil {
+		if k8sErrors.IsNotFound(err) {
+			return issue(ctx, i, crt)
+		}
+		return err
+	}
 
-	// certBytes, okcert := secret.Data[api.TLSCertKey]
-	// keyBytes, okkey := secret.Data[api.TLSPrivateKeyKey]
+	certBytes, okcert := secret.Data[api.TLSCertKey]
+	keyBytes, okkey := secret.Data[api.TLSPrivateKeyKey]
 
-	// // check if the certificate and private key exist, if not, trigger an issue
-	// if !okcert || !okkey {
-	// 	return c.issue(crt)
-	// }
-	// // decode the tls certificate pem
-	// block, _ := pem.Decode(certBytes)
-	// if block == nil {
-	// 	ctx.Logger.Printf("error decoding cert PEM block in '%s'", crt.Spec.SecretName)
-	// 	return c.issue(crt)
-	// }
-	// // parse the tls certificate
-	// cert, err := x509.ParseCertificate(block.Bytes)
-	// if err != nil {
-	// 	ctx.Logger.Printf("error parsing TLS certificate in '%s': %s", crt.Spec.SecretName, err.Error())
-	// 	return c.issue(crt)
-	// }
-	// // decode the private key pem
-	// block, _ = pem.Decode(keyBytes)
-	// if block == nil {
-	// 	ctx.Logger.Printf("error decoding private key PEM block in '%s'", crt.Spec.SecretName)
-	// 	return c.issue(crt)
-	// }
-	// // parse the private key
-	// key, err := x509.ParsePKCS1PrivateKey(block.Bytes)
-	// if err != nil {
-	// 	ctx.Logger.Printf("error parsing private key in '%s': %s", crt.Spec.SecretName, err.Error())
-	// 	return c.issue(crt)
-	// }
-	// // validate the private key
-	// if err = key.Validate(); err != nil {
-	// 	ctx.Logger.Printf("private key failed validation in '%s': %s", crt.Spec.SecretName, err.Error())
-	// 	return c.issue(crt)
-	// }
-	// // step two: check if referenced secret is valid for listed domains. if not, return failure
-	// if !equalUnsorted(crt.Spec.Domains, cert.DNSNames) {
-	// 	ctx.Logger.Printf("list of domains on certificate do not match domains in spec")
-	// 	return c.issue(crt)
-	// }
-	// // step three: check if referenced secret is valid (after start & before expiry)
+	// check if the certificate and private key exist, if not, trigger an issue
+	if !okcert || !okkey {
+		return issue(ctx, i, crt)
+	}
+	// decode the tls certificate pem
+	block, _ := pem.Decode(certBytes)
+	if block == nil {
+		ctx.Logger.Printf("error decoding cert PEM block in '%s'", crt.Spec.SecretName)
+		return issue(ctx, i, crt)
+	}
+	// parse the tls certificate
+	cert, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		ctx.Logger.Printf("error parsing TLS certificate in '%s': %s", crt.Spec.SecretName, err.Error())
+		return issue(ctx, i, crt)
+	}
+	// decode the private key pem
+	block, _ = pem.Decode(keyBytes)
+	if block == nil {
+		ctx.Logger.Printf("error decoding private key PEM block in '%s'", crt.Spec.SecretName)
+		return issue(ctx, i, crt)
+	}
+	// parse the private key
+	key, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		ctx.Logger.Printf("error parsing private key in '%s': %s", crt.Spec.SecretName, err.Error())
+		return issue(ctx, i, crt)
+	}
+	// validate the private key
+	if err = key.Validate(); err != nil {
+		ctx.Logger.Printf("private key failed validation in '%s': %s", crt.Spec.SecretName, err.Error())
+		return issue(ctx, i, crt)
+	}
+	// step two: check if referenced secret is valid for listed domains. if not, return failure
+	if !equalUnsorted(crt.Spec.Domains, cert.DNSNames) {
+		ctx.Logger.Printf("list of domains on certificate do not match domains in spec")
+		return issue(ctx, i, crt)
+	}
+	// step three: check if referenced secret is valid (after start & before expiry)
 	// if time.Now().Sub(cert.NotAfter) > time.Hour*(24*30) {
 	// 	return c.renew(crt)
 	// }
@@ -90,38 +98,33 @@ func sync(ctx *controller.Context, crt *v1alpha1.Certificate) error {
 	return nil
 }
 
-// // issue will attempt to retrieve a certificate from the specified issuer, or
-// // return an error on failure. If retrieval is succesful, the certificate data
-// // and private key will be stored in the named secret
-// func (c *Controller) issue(crt *v1alpha1.Certificate) error {
-// 	i, err := issuer.IssuerFor(crt)
-// 	if err != nil {
-// 		return err
-// 	}
+// issue will attempt to retrieve a certificate from the specified issuer, or
+// return an error on failure. If retrieval is succesful, the certificate data
+// and private key will be stored in the named secret
+func issue(ctx *controller.Context, issuer issuer.Interface, crt *v1alpha1.Certificate) error {
+	cert, key, err := issuer.Issue(crt)
+	if err != nil {
+		return fmt.Errorf("error issuing certificate: %s", err.Error())
+	}
 
-// 	cert, key, err := i.Issue(&ctx, crt)
-// 	if err != nil {
-// 		return fmt.Errorf("error issuing certificate: %s", err.Error())
-// 	}
+	// TODO: support updating resources
+	_, err = ctx.Client.Secrets(crt.Namespace).Create(&api.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      crt.Spec.SecretName,
+			Namespace: crt.Namespace,
+		},
+		Data: map[string][]byte{
+			api.TLSCertKey:       cert,
+			api.TLSPrivateKeyKey: key,
+		},
+	})
 
-// 	// TODO: support updating resources
-// 	_, err = ctx.Client.Secrets(crt.Namespace).Create(&api.Secret{
-// 		ObjectMeta: metav1.ObjectMeta{
-// 			Name:      crt.Spec.SecretName,
-// 			Namespace: crt.Namespace,
-// 		},
-// 		Data: map[string][]byte{
-// 			api.TLSCertKey:       cert,
-// 			api.TLSPrivateKeyKey: key,
-// 		},
-// 	})
+	if err != nil {
+		return fmt.Errorf("error saving certificate: %s", err.Error())
+	}
 
-// 	if err != nil {
-// 		return fmt.Errorf("error saving certificate: %s", err.Error())
-// 	}
-
-// 	return nil
-// }
+	return nil
+}
 
 // // renew will attempt to renew a certificate from the specified issuer, or
 // // return an error on failure. If renewal is succesful, the certificate data
@@ -155,17 +158,17 @@ func sync(ctx *controller.Context, crt *v1alpha1.Certificate) error {
 // 	return nil
 // }
 
-// func equalUnsorted(s1 []string, s2 []string) bool {
-// 	if len(s1) != len(s2) {
-// 		return false
-// 	}
-// 	s1_2, s2_2 := make([]string, len(s1)), make([]string, len(s2))
-// 	sort.Strings(s1)
-// 	sort.Strings(s2)
-// 	for i, s := range s1_2 {
-// 		if s != s2_2[i] {
-// 			return false
-// 		}
-// 	}
-// 	return true
-// }
+func equalUnsorted(s1 []string, s2 []string) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+	s1_2, s2_2 := make([]string, len(s1)), make([]string, len(s2))
+	sort.Strings(s1)
+	sort.Strings(s2)
+	for i, s := range s1_2 {
+		if s != s2_2[i] {
+			return false
+		}
+	}
+	return true
+}
--- pkg/issuer/acme/issue.go
+++ pkg/issuer/acme/issue.go
@@ -1,11 +1,76 @@
 package acme
 
 import (
+	"bytes"
+	"context"
+	"crypto/rand"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
 	"fmt"
+
+	"golang.org/x/crypto/acme"
 
 	"github.com/jetstack/cert-manager/pkg/apis/certmanager/v1alpha1"
 )
 
 func (a *Acme) Issue(crt *v1alpha1.Certificate) ([]byte, []byte, error) {
-	return nil, nil, fmt.Errorf("not implemented")
+	if crt.Spec.ACME == nil {
+		return nil, nil, fmt.Errorf("acme config must be specified")
+	}
+	domains := crt.Spec.Domains
+
+	if len(domains) == 0 {
+		return nil, nil, fmt.Errorf("no domains specified")
+	}
+
+	privKey, err := a.account.privateKey()
+
+	if err != nil {
+		return nil, nil, fmt.Errorf("error getting acme account private key: %s", err.Error())
+	}
+
+	cl := &acme.Client{
+		Key:          privKey,
+		DirectoryURL: a.account.server(),
+	}
+
+	template := x509.CertificateRequest{
+		Subject: pkix.Name{
+			CommonName: domains[0],
+		},
+	}
+
+	if len(domains) > 1 {
+		template.DNSNames = domains
+	}
+
+	privateKeyPem, privateKey, err := generatePrivateKey(2048)
+	if err != nil {
+		return nil, nil, fmt.Errorf("error generating private key for certificiate '%s': %s", crt.Name, err)
+	}
+
+	csr, err := x509.CreateCertificateRequest(rand.Reader, &template, privateKey)
+	if err != nil {
+		return nil, nil, fmt.Errorf("error certificate request: %s", err)
+	}
+
+	certSlice, certUrl, err := cl.CreateCert(
+		context.Background(),
+		csr,
+		0,
+		true,
+	)
+	if err != nil {
+		return nil, nil, fmt.Errorf("error getting certificate for acme server: %s", err)
+	}
+
+	certBuffer := bytes.NewBuffer([]byte{})
+	for _, cert := range certSlice {
+		pem.Encode(certBuffer, &pem.Block{Type: "CERTIFICATE", Bytes: cert})
+	}
+
+	a.ctx.Logger.Printf("successfully got certificate: domains=%+v url=%s", domains, certUrl)
+
+	return privateKeyPem, certBuffer.Bytes(), nil
 }
