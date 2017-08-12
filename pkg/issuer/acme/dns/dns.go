32dff6079c652c3206268107daf8408a0fa85f72
Christian Simon <simon@swine.de>
2017-08-12 14:54:53 +0100

    Fix secret access key link

--- pkg/issuer/acme/dns/dns.go
+++ pkg/issuer/acme/dns/dns.go
@@ -139,7 +139,7 @@ func (s *Solver) solverFor(crt *v1alpha1.Certificate, domain string) (solver, er
 			return nil, fmt.Errorf("error getting route53 secret access key: %s", err.Error())
 		}
 
-		secretAccessKeyBytes, ok := secretAccessKeySecret.Data[providerConfig.Cloudflare.APIKey.Key]
+		secretAccessKeyBytes, ok := secretAccessKeySecret.Data[providerConfig.Route53.SecretAccessKey.Key]
 		if !ok {
 			return nil, fmt.Errorf("error getting route53 secret access key: key '%s' not found in secret", providerConfig.Route53.SecretAccessKey.Key)
 		}
