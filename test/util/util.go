9e6bd026e33f8f7d7a0028d6e9ca276e7195a2b7
James Munnelly <james.munnelly@jetstack.io>
2017-09-08 22:51:01 +0100

    Wait for CRD to no longer exist after each test

--- test/util/util.go
+++ test/util/util.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/golang/glog"
+	apiextcs "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset/typed/apiextensions/v1beta1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 
-	"github.com/golang/glog"
 	"github.com/jetstack-experimental/cert-manager/pkg/apis/certmanager/v1alpha1"
 	clientset "github.com/jetstack-experimental/cert-manager/pkg/client/typed/certmanager/v1alpha1"
 )
@@ -24,6 +26,26 @@ func WaitForIssuerCondition(client clientset.IssuerInterface, name string, condi
 			}
 
 			return v1alpha1.IssuerHasCondition(issuer, condition), nil
+		},
+	)
+}
+
+// WaitForCRDToNotExist waits for the CRD with the given name to no
+// longer exist.
+func WaitForCRDToNotExist(client apiextcs.CustomResourceDefinitionInterface, name string) error {
+	return wait.PollImmediate(500*time.Millisecond, wait.ForeverTestTimeout,
+		func() (bool, error) {
+			glog.V(5).Infof("Waiting for CRD %v to not exist", name)
+			_, err := client.Get(name, metav1.GetOptions{})
+			if nil == err {
+				return false, nil
+			}
+
+			if errors.IsNotFound(err) {
+				return true, nil
+			}
+
+			return false, nil
 		},
 	)
 }
