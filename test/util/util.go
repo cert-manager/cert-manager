6d302c57e3fe3c0e6046e6454fac317325e56aea
James Munnelly <james.munnelly@jetstack.io>
2017-09-08 22:27:10 +0100

    Fix tests for conditions. Add test for failed registration.

--- test/util/util.go
+++ test/util/util.go
@@ -7,19 +7,30 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 
+	"github.com/golang/glog"
+	"github.com/jetstack-experimental/cert-manager/pkg/apis/certmanager/v1alpha1"
 	clientset "github.com/jetstack-experimental/cert-manager/pkg/client/typed/certmanager/v1alpha1"
 )
 
-func WaitForIssuerReady(cl clientset.IssuerInterface, name string) error {
+// WaitForIssuerCondition waits for the status of the named issuer to contain
+// a condition whose type and status matches the supplied one.
+func WaitForIssuerCondition(client clientset.IssuerInterface, name string, condition v1alpha1.IssuerCondition) error {
 	return wait.PollImmediate(500*time.Millisecond, wait.ForeverTestTimeout,
 		func() (bool, error) {
-			issuer, err := cl.Get(name, metav1.GetOptions{})
+			glog.V(5).Infof("Waiting for issuer %v condition %#v", name, condition)
+			issuer, err := client.Get(name, metav1.GetOptions{})
 			if nil != err {
-				return false, fmt.Errorf("error getting Broker %v: %v", name, err)
+				return false, fmt.Errorf("error getting Issuer %v: %v", name, err)
 			}
 
-			if issuer.Status.Ready {
-				return true, nil
+			if len(issuer.Status.Conditions) == 0 {
+				return false, nil
+			}
+
+			for _, cond := range issuer.Status.Conditions {
+				if condition.Type == cond.Type && condition.Status == cond.Status {
+					return true, nil
+				}
 			}
 
 			return false, nil
