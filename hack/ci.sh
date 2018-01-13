d5dd212cab9cfcd8353c34375c2955138eb9e08e
James Munnelly <james.munnelly@jetstack.io>
2018-01-13 15:43:59 +0000

    Setup minikube on travis, don't install docker-compose

--- hack/ci.sh
+++ hack/ci.sh
@@ -3,9 +3,6 @@
 ## This file is a stop gap whilst we migrate the Makefile to better
 ## supprt prow for our testing
 
-curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
 # Build images while we wait for services to start
 make build APP_VERSION=build
 
