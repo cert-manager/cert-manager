47ccc0240bc0ca7a3d2b5d3ad1d8cec5a6acb257
James Munnelly <james.munnelly@jetstack.io>
2018-01-13 02:58:52 +0000

    Install docker-compose

--- hack/ci.sh
+++ hack/ci.sh
@@ -3,6 +3,9 @@
 ## This file is a stop gap whilst we migrate the Makefile to better
 ## supprt prow for our testing
 
+curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
 # Setup e2e service dependencies
 ./hack/test/setup-boulder.sh
 
