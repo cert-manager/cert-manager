25d278e446b870f3ba271452d2807c98f137e99f
Pete Brown <pete.brown@powerhrg.com>
2018-01-16 10:23:40 -0500

    Add ConfigMap for custom dns servers for cert-manager
    Add option to add ImagePullSecrets to deployemnt
    
    Fix pkg/issuer/acme/dns/util/wait.go code so custom DNS servers work

--- pkg/issuer/acme/dns/util/wait.go
+++ pkg/issuer/acme/dns/util/wait.go
@@ -22,11 +22,7 @@ var (
 )
 
 var defaultResolvConf = "/etc/resolv.conf"
-var acmeResolvConf    = "/etc/resolv.acme.conf"
-
-if _, err := os.Stat(acmeResolvConf); err == nil {
-  defaultResolvConf = acmeResolvConf
-}
+var acmeResolvConf    = "/etc/resolv_acme.conf"
 
 var defaultNameservers = []string{
 	"google-public-dns-a.google.com:53",
@@ -39,8 +35,13 @@ var RecursiveNameservers = getNameservers(defaultResolvConf, defaultNameservers)
 var DNSTimeout = 10 * time.Second
 
 // getNameservers attempts to get systems nameservers before falling back to the defaults
-func getNameservers(path string, defaults []string) []string {
-	config, err := dns.ClientConfigFromFile(path)
+func getNameservers(resolvConf string, defaults []string) []string {
+
+	if _, err := os.Stat(acmeResolvConf); err == nil {
+	  resolvConf = acmeResolvConf
+	}
+
+	config, err := dns.ClientConfigFromFile(resolvConf)
 	if err != nil || len(config.Servers) == 0 {
 		return defaults
 	}
