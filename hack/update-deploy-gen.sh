0d3790567a539c099e746181a8f6f776951de4de
James Munnelly <james.munnelly@jetstack.io>
2018-01-24 19:04:32 +0000

    Add deployment manifest generation scripts

---
+++ hack/update-deploy-gen.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+SCRIPT_ROOT=$(dirname ${BASH_SOURCE})
+REPO_ROOT="${SCRIPT_ROOT}/.."
+# This will set Capabilities.KubeVersion.Major/Minor when generating manifests
+KUBE_VERSION=1.9
+
+gen() {
+	VALUES=$1
+	OUTPUT_DIR=$2
+	TMP_OUTPUT=$(mktemp -d)
+	mkdir -p "${OUTPUT_DIR}"
+	helm template \
+		"${REPO_ROOT}/contrib/charts/cert-manager" \
+		--values "${SCRIPT_ROOT}/deploy/${VALUES}.yaml" \
+		--kube-version "${KUBE_VERSION}" \
+		--namespace "default" \
+		--name "cert-manager" \
+		--set "fullnameOverride=cert-manager" \
+		--output-dir "${TMP_OUTPUT}"
+	mv "${TMP_OUTPUT}"/cert-manager/templates/*.* "${OUTPUT_DIR}/"
+}
+
+gen rbac-values "${REPO_ROOT}/docs/deploy/rbac"
+gen without-rbac-values "${REPO_ROOT}/docs/deploy/without-rbac"
