296f6e334cb16cd36d73f44953b17f80aa1b4939
James Munnelly <munnerz@users.noreply.github.com>
2018-01-24 17:17:41 +0000

    Remove unused file

--- pkg/apis/certmanager/v1alpha1/annotations.go
+++
@@ -1,8 +0,0 @@
-package v1alpha1
-
-const (
-	// AnnotationIngressACMETLS is an annotation that can be added to an
-	// ingress resource that signals the resource should have an automatically
-	// provisioned TLS certificate
-	AnnotationIngressACMETLS = "certmanager.kubernetes.io/enabled"
-)
