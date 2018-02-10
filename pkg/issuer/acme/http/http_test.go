a2ad31c84905e4f340cf0340247df3d0769a329f
James Munnelly <james.munnelly@jetstack.io>
2018-02-10 01:29:24 +0000

    Fix acme http test and remove old acme prepare test

--- pkg/issuer/acme/http/http_test.go
+++ pkg/issuer/acme/http/http_test.go
@@ -16,30 +16,38 @@ func contextWithTimeout(t time.Duration) context.Context {
 // countReachabilityTestCalls is a wrapper function that allows us to count the number
 // of calls to a reachabilityTest.
 func countReachabilityTestCalls(counter *int, t reachabilityTest) reachabilityTest {
-	return func(ctx context.Context, domain, path, key string) error {
+	return func(ctx context.Context, domain, path, key string) (bool, error) {
 		*counter++
 		return t(ctx, domain, path, key)
 	}
 }
 
-func TestWait(t *testing.T) {
+func TestCheck(t *testing.T) {
 	type testT struct {
 		name               string
-		reachabilityTest   func(ctx context.Context, domain, path, key string) error
+		reachabilityTest   reachabilityTest
 		domain, token, key string
 		expectedErr        bool
+		expectedOk         bool
 	}
 	tests := []testT{
 		{
 			name: "should pass",
-			reachabilityTest: func(context.Context, string, string, string) error {
-				return nil
+			reachabilityTest: func(context.Context, string, string, string) (bool, error) {
+				return true, nil
 			},
+			expectedOk: true,
 		},
 		{
 			name: "should fail",
-			reachabilityTest: func(context.Context, string, string, string) error {
-				return fmt.Errorf("failed")
+			reachabilityTest: func(context.Context, string, string, string) (bool, error) {
+				return false, nil
+			},
+		},
+		{
+			name: "should error",
+			reachabilityTest: func(context.Context, string, string, string) (bool, error) {
+				return false, fmt.Errorf("failed")
 			},
 			expectedErr: true,
 		},
@@ -55,7 +63,7 @@ func TestWait(t *testing.T) {
 				requiredPasses:   requiredCallsForPass,
 			}
 
-			err := s.Check(test.domain, test.token, test.key)
+			ok, err := s.Check(test.domain, test.token, test.key)
 			if err != nil && !test.expectedErr {
 				t.Errorf("Expected Check to return non-nil error, but got %v", err)
 				return
@@ -64,7 +72,10 @@ func TestWait(t *testing.T) {
 				t.Errorf("Expected error from Check, but got none")
 				return
 			}
-			if test.expectedErr == false && calls != requiredCallsForPass {
+			if test.expectedOk != ok {
+				t.Errorf("Expected ok=%t but got ok=%t", test.expectedOk, ok)
+			}
+			if test.expectedOk && calls != requiredCallsForPass {
 				t.Errorf("Expected Wait to verify reachability test passes %d times, but only checked %d", requiredCallsForPass, calls)
 				return
 			}
