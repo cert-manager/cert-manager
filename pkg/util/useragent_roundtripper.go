6b4e33a483b7134b4c1ef3fd45fc9bb895f3490f
Euan Kemp <euank@euank.com>
2018-04-04 16:18:37 -0700

    util/useragent: use more verbose version

--- pkg/util/useragent_roundtripper.go
+++ pkg/util/useragent_roundtripper.go
@@ -4,19 +4,19 @@ import (
 	"net/http"
 )
 
+// CertManagerUserAgent is the user agent that http clients in this codebase should use
+var CertManagerUserAgent = "jetstack-cert-manager/" + version()
+
 // UserAgentRoundTripper implements the http.RoundTripper interface and adds a User-Agent
 // header.
 type userAgentRoundTripper struct {
 	inner http.RoundTripper
 }
 
-// CertManagerUserAgent is the user agent that http clients in this codebase should use
-const CertManagerUserAgent = "jetstack-cert-manager/" + AppVersion
-
 // UserAgentRoundTripper returns a RoundTripper that functions identically to
 // the provided 'inner' round tripper, other than also setting a user agent.
 func UserAgentRoundTripper(inner http.RoundTripper) http.RoundTripper {
-	return UserAgentRoundTripper{
+	return userAgentRoundTripper{
 		inner: inner,
 	}
 }
