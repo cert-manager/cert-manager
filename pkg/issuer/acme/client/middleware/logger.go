8f2bab6f05d99f4570b37c542709b3f16c203110
James Munnelly <james.munnelly@jetstack.io>
2018-04-09 19:09:46 +0100

    Fix infinite loop in logger middleware

--- pkg/issuer/acme/client/middleware/logger.go
+++ pkg/issuer/acme/client/middleware/logger.go
@@ -20,55 +20,55 @@ type Logger struct {
 
 func (l *Logger) CreateOrder(ctx context.Context, order *acme.Order) (*acme.Order, error) {
 	glog.Infof("Calling CreateOrder")
-	return l.CreateOrder(ctx, order)
+	return l.baseCl.CreateOrder(ctx, order)
 }
 
 func (l *Logger) GetOrder(ctx context.Context, url string) (*acme.Order, error) {
 	glog.Infof("Calling GetOrder")
-	return l.GetOrder(ctx, url)
+	return l.baseCl.GetOrder(ctx, url)
 }
 
 func (l *Logger) FinalizeOrder(ctx context.Context, finalizeURL string, csr []byte) (der [][]byte, err error) {
 	glog.Infof("Calling FinalizeOrder")
-	return l.FinalizeOrder(ctx, finalizeURL, csr)
+	return l.baseCl.FinalizeOrder(ctx, finalizeURL, csr)
 }
 
 func (l *Logger) AcceptChallenge(ctx context.Context, chal *acme.Challenge) (*acme.Challenge, error) {
 	glog.Infof("Calling AcceptChallenge")
-	return l.AcceptChallenge(ctx, chal)
+	return l.baseCl.AcceptChallenge(ctx, chal)
 }
 
 func (l *Logger) GetChallenge(ctx context.Context, url string) (*acme.Challenge, error) {
 	glog.Infof("Calling GetChallenge")
-	return l.GetChallenge(ctx, url)
+	return l.baseCl.GetChallenge(ctx, url)
 }
 
 func (l *Logger) GetAuthorization(ctx context.Context, url string) (*acme.Authorization, error) {
 	glog.Infof("Calling GetAuthorization")
-	return l.GetAuthorization(ctx, url)
+	return l.baseCl.GetAuthorization(ctx, url)
 }
 
 func (l *Logger) WaitAuthorization(ctx context.Context, url string) (*acme.Authorization, error) {
 	glog.Infof("Calling WaitAuthorization")
-	return l.WaitAuthorization(ctx, url)
+	return l.baseCl.WaitAuthorization(ctx, url)
 }
 
 func (l *Logger) CreateAccount(ctx context.Context, a *acme.Account) (*acme.Account, error) {
 	glog.Infof("Calling CreateAccount")
-	return l.CreateAccount(ctx, a)
+	return l.baseCl.CreateAccount(ctx, a)
 }
 
 func (l *Logger) GetAccount(ctx context.Context) (*acme.Account, error) {
 	glog.Infof("Calling GetAccount")
-	return l.GetAccount(ctx)
+	return l.baseCl.GetAccount(ctx)
 }
 
 func (l *Logger) HTTP01ChallengeResponse(token string) (string, error) {
 	glog.Infof("Calling HTTP01ChallengeResponse")
-	return l.HTTP01ChallengeResponse(token)
+	return l.baseCl.HTTP01ChallengeResponse(token)
 }
 
 func (l *Logger) DNS01ChallengeRecord(token string) (string, error) {
 	glog.Infof("Calling DNS01ChallengeRecord")
-	return l.DNS01ChallengeRecord(token)
+	return l.baseCl.DNS01ChallengeRecord(token)
 }
