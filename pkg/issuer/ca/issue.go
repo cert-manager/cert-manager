1fd8cdf13e2bd01506b98e6339d6649ee50694dd
James Munnelly <james.munnelly@jetstack.io>
2018-06-08 15:11:48 +0100

    Create common GenerateCSR and GenerateTemplate methods for creating Certificate/CertificateRequest

--- pkg/issuer/ca/issue.go
+++ pkg/issuer/ca/issue.go
@@ -1,15 +1,8 @@
 package ca
 
 import (
-	"bytes"
 	"context"
-	"crypto/rand"
-	"crypto/x509"
-	"crypto/x509/pkix"
-	"encoding/pem"
 	"fmt"
-	"math/big"
-	"time"
 
 	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
 
@@ -29,12 +22,6 @@ const (
 	messageErrorIssueCert      = "Error issuing TLS certificate: "
 
 	messageCertIssued = "Certificate issued successfully"
-)
-
-const (
-	// certificateDuration of 1 year
-	certificateDuration = time.Hour * 24 * 365
-	defaultOrganization = "cert-manager"
 )
 
 func (c *CA) Issue(ctx context.Context, crt *v1alpha1.Certificate) ([]byte, []byte, error) {
@@ -80,78 +67,15 @@ func (c *CA) obtainCertificate(crt *v1alpha1.Certificate, signeeKey interface{})
 		return nil, fmt.Errorf("error getting issuer private key: %s", err.Error())
 	}
 
-	crtPem, _, err := signCertificate(crt, signerCert, signeeKey, signerKey)
+	template, err := pki.GenerateTemplate(c.issuer, crt, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	crtPem, _, err := pki.SignCertificate(template, signerCert, signeeKey, signerKey)
 	if err != nil {
 		return nil, err
 	}
 
 	return crtPem, nil
-}
-
-func createCertificateTemplate(publicKey interface{}, commonName string, altNames ...string) (*x509.Certificate, error) {
-	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
-	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
-	if err != nil {
-		return nil, fmt.Errorf("failed to generate serial number: %s", err.Error())
-	}
-	if len(commonName) == 0 && len(altNames) > 0 {
-		commonName = altNames[0]
-	}
-	cert := &x509.Certificate{
-		Version:               3,
-		BasicConstraintsValid: true,
-		SerialNumber:          serialNumber,
-		SignatureAlgorithm:    x509.SHA256WithRSA,
-		PublicKey:             publicKey,
-		Subject: pkix.Name{
-			Organization: []string{defaultOrganization},
-			CommonName:   commonName,
-		},
-		NotBefore: time.Now(),
-		NotAfter:  time.Now().Add(certificateDuration),
-		// see http://golang.org/pkg/crypto/x509/#KeyUsage
-		KeyUsage: x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment,
-		DNSNames: altNames,
-	}
-	return cert, nil
-}
-
-// signCertificate returns a signed x509.Certificate object for the given
-// *v1alpha1.Certificate crt.
-// publicKey is the public key of the signee, and signerKey is the private
-// key of the signer.
-func signCertificate(crt *v1alpha1.Certificate, issuerCert *x509.Certificate, publicKey interface{}, signerKey interface{}) ([]byte, *x509.Certificate, error) {
-	cn := pki.CommonNameForCertificate(crt)
-	dnsNames := pki.DNSNamesForCertificate(crt)
-
-	template, err := createCertificateTemplate(publicKey, cn, dnsNames...)
-	if err != nil {
-		return nil, nil, fmt.Errorf("error creating x509 certificate template: %s", err.Error())
-	}
-
-	derBytes, err := x509.CreateCertificate(rand.Reader, template, issuerCert, publicKey, signerKey)
-
-	if err != nil {
-		return nil, nil, fmt.Errorf("error creating x509 certificate: %s", err.Error())
-	}
-
-	cert, err := pki.DecodeDERCertificateBytes(derBytes)
-
-	if err != nil {
-		return nil, nil, fmt.Errorf("error decoding DER certificate bytes: %s", err.Error())
-	}
-
-	pemBytes := bytes.NewBuffer([]byte{})
-	err = pem.Encode(pemBytes, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
-	if err != nil {
-		return nil, nil, fmt.Errorf("error encoding certificate PEM: %s", err.Error())
-	}
-
-	// bundle the CA
-	err = pem.Encode(pemBytes, &pem.Block{Type: "CERTIFICATE", Bytes: issuerCert.Raw})
-	if err != nil {
-		return nil, nil, fmt.Errorf("error encoding issuer cetificate PEM: %s", err.Error())
-	}
-
-	return pemBytes.Bytes(), cert, err
 }
