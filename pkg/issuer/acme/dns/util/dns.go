ae08cc19ffc20d8695fad15c114ae0bc336b7841
Gurvinder Singh <gurvinder.singh@uninett.no>
2018-06-21 21:48:16 +0200

    add support CNAME for dns-01 challenge
    Domain for which certificate is asked for can have a CNAME, so we should check it.
    If domain has a CNAME, create the challange TXT record in the alias domain.
    
    This is useful in the scenario where a company like us is using some DNS provider
    which is not supported dynamically. We can then create a CNAME for records like
    
    _acme-challenge.example.com -> example.aws.hosted.com
    
    So this will allow us getting cert for *.example.com with creating txt record in route53 for above exxample.

--- pkg/issuer/acme/dns/util/dns.go
+++ pkg/issuer/acme/dns/util/dns.go
@@ -1,9 +1,20 @@
 package util
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/miekg/dns"
+)
 
 // DNS01Record returns a DNS record which will fulfill the `dns-01` challenge
 // TODO: move this into a non-generic place by resolving import cycle in dns package
 func DNS01Record(domain, value string) (string, string, int) {
-	return fmt.Sprintf("_acme-challenge.%s.", domain), value, 60
+	fqdn := fmt.Sprintf("_acme-challenge.%s.", domain)
+
+	// Check if the domain has CNAME then return that
+	r, err := dnsQuery(fqdn, dns.TypeCNAME, RecursiveNameservers, true)
+	if err == nil && r.Rcode == dns.RcodeSuccess {
+		fqdn = updateDomainWithCName(r, fqdn)
+	}
+	return fqdn, value, 60
 }
