009aebb62af0de893a8c1da77692383ebb7924b7
James Munnelly <james.munnelly@jetstack.io>
2018-07-04 14:33:38 +0100

    Default and set REMOTE during chart test

--- hack/verify-chart-version.sh
+++ hack/verify-chart-version.sh
@@ -5,10 +5,21 @@ set -o nounset
 set -o pipefail
 
 readonly REPO_ROOT=$(git rev-parse --show-toplevel)
+readonly UPSTREAM_REPO="https://github.com/jetstack/cert-manager.git"
 
 if [ -z "${PULL_BASE_REF:-}" ]; then
     echo "PULL_BASE_REF must be set to a target branch name"
     exit 1
+fi
+
+if [ -z "${REMOTE:-}" ]; then
+    echo "+++ REMOTE not set - defaulting to 'upstream'"
+    export REMOTE="upstream"
+fi
+
+if [ ! git remote get-url "${REMOTE}" 2>&1 /dev/null ]; then
+    echo "+++ Remote '${REMOTE}' does not exist. Setting to "
+    git remote add "${REMOTE}" "${UPSTREAM_REPO}"
 fi
 
 docker run --rm -v "${REPO_ROOT}:/workdir" --workdir /workdir -e TARGET_BRANCH="${PULL_BASE_REF}" \
