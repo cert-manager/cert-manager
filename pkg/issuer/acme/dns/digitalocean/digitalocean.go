ccd5d21fcb8e7609b972189d28ad4c2168dfde02
Daniel Beal <git@danb.email>
2018-07-28 19:16:09 -0700

    Update DigitalOcean DNS01 challenge to work with latest APIs
    Also update to use GoDo as requested
    
    Signed-off-by: Daniel Beal <git@danb.email>
    Signed-off-by: Louis Taylor <louis@kragniz.eu>

--- pkg/issuer/acme/dns/digitalocean/digitalocean.go
+++ pkg/issuer/acme/dns/digitalocean/digitalocean.go
@@ -3,23 +3,20 @@
 package digitalocean
 
 import (
-	"bytes"
-	"encoding/json"
+	"context"
 	"fmt"
-	"io"
-	"net/http"
 	"os"
 	"time"
 
+	"github.com/digitalocean/godo"
 	"github.com/jetstack/cert-manager/pkg/issuer/acme/dns/util"
-)
 
-// DigitalOceanAPIURL represents the API endpoint to call.
-const DigitalOceanAPIURL = "https://api.digitalocean.com/v2"
+	"golang.org/x/oauth2"
+)
 
 // DNSProvider is an implementation of the acme.ChallengeProvider interface
 type DNSProvider struct {
-	authToken string
+	client *godo.Client
 }
 
 // NewDNSProvider returns a DNSProvider instance configured for digitalocean.
@@ -36,8 +33,13 @@ func NewDNSProviderCredentials(token string) (*DNSProvider, error) {
 		return nil, fmt.Errorf("DigitalOcean token missing")
 	}
 
+	c := oauth2.NewClient(
+		context.Background(),
+		oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token}),
+	)
+
 	return &DNSProvider{
-		authToken: token,
+		client: godo.NewClient(c),
 	}, nil
 }
 
@@ -49,7 +51,7 @@ func (c *DNSProvider) Timeout() (timeout, interval time.Duration) {
 
 // Present creates a TXT record to fulfil the dns-01 challenge
 func (c *DNSProvider) Present(domain, token, keyAuth string) error {
-	fqdn, value, _ := util.DNS01Record(domain, keyAuth)
+	fqdn, value, ttl := util.DNS01Record(domain, keyAuth)
 
 	// if DigitalOcean does not have this zone then we will find out later
 	zoneName, err := util.FindZoneByFqdn(fqdn, util.RecursiveNameservers)
@@ -57,19 +59,19 @@ func (c *DNSProvider) Present(domain, token, keyAuth string) error {
 		return err
 	}
 
-	rec := digitalOceanRecord{
+	createRequest := &godo.DomainRecordEditRequest{
 		Type: "TXT",
-		Name: util.UnFqdn(fqdn),
+		Name: fqdn,
 		Data: value,
-		TTL:  120,
+		TTL:  ttl,
 	}
 
-	body, err := json.Marshal(rec)
-	if err != nil {
-		return err
-	}
+	_, _, err = c.client.Domains.CreateRecord(
+		context.Background(),
+		util.UnFqdn(zoneName),
+		createRequest,
+	)
 
-	_, err = c.makeRequest("POST", fmt.Sprintf("/domains/%s/records", util.UnFqdn(zoneName)), bytes.NewReader(body))
 	if err != nil {
 		return err
 	}
@@ -83,108 +85,42 @@ func (c *DNSProvider) CleanUp(domain, token, keyAuth string) error {
 
 	zoneName, err := util.FindZoneByFqdn(fqdn, util.RecursiveNameservers)
 
-	record, err := c.findTxtRecord(fqdn)
+	records, err := c.findTxtRecord(fqdn)
 	if err != nil {
 		return err
 	}
 
-	_, err = c.makeRequest("DELETE", fmt.Sprintf("/domains/%s/records/%d", util.UnFqdn(zoneName), record.ID), nil)
-	if err != nil {
-		return err
+	for _, record := range records {
+		_, err = c.client.Domains.DeleteRecord(context.Background(), util.UnFqdn(zoneName), record.ID)
+
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
 }
 
-func (c *DNSProvider) findTxtRecord(fqdn string) (*digitalOceanRecord, error) {
-
-	// APIDomainRecordsResponse represents a domain records list response from the digitalocean API
-	type APIDomainRecordsResponse struct {
-		DomainRecords []digitalOceanRecord `json:"domain_records"`
-	}
+func (c *DNSProvider) findTxtRecord(fqdn string) ([]godo.DomainRecord, error) {
 
 	zoneName, err := util.FindZoneByFqdn(fqdn, util.RecursiveNameservers)
 	if err != nil {
 		return nil, err
 	}
 
-	result, err := c.makeRequest(
-		"GET",
-		fmt.Sprintf("/domains/%s/records?per_page=1000&type=TXT&name=%s", util.UnFqdn(zoneName), util.UnFqdn(fqdn)),
+	allRecords, _, err := c.client.Domains.Records(
+		context.Background(),
+		util.UnFqdn(zoneName),
 		nil,
 	)
-	if err != nil {
-		return nil, err
-	}
 
-	var records APIDomainRecordsResponse
-	err = json.Unmarshal(result, &records)
-	if err != nil {
-		return nil, err
-	}
+	var records []godo.DomainRecord
 
-	for _, rec := range records.DomainRecords {
-		if rec.Name == util.UnFqdn(fqdn) {
-			return &rec, nil
+	for _, record := range allRecords {
+		if util.ToFqdn(record.Name) == fqdn {
+			records = append(records, record)
 		}
 	}
 
-	return nil, fmt.Errorf("No existing record found for %s", fqdn)
-}
-
-func (c *DNSProvider) makeRequest(method, uri string, body io.Reader) (json.RawMessage, error) {
-	// APIError contains error details for failed requests
-	type APIError struct {
-		Code    int    `json:"code,omitempty"`
-		Message string `json:"message,omitempty"`
-	}
-
-	req, err := http.NewRequest(method, fmt.Sprintf("%s%s", DigitalOceanAPIURL, uri), body)
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.authToken))
-	req.Header.Set("Content-Type", "application/json")
-	//req.Header.Set("User-Agent", userAgent())
-
-	client := http.Client{Timeout: 30 * time.Second}
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("Error querying DigitalOcean API -> %v", err)
-	}
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode >= 400 {
-		var r APIError
-
-		err = json.NewDecoder(resp.Body).Decode(&r)
-		if err != nil {
-			return nil, err
-		}
-
-		return nil, fmt.Errorf("DigitalOcean API Error \n%d: %s", r.Code, r.Message)
-	}
-
-	if method != "DELETE" {
-		var res json.RawMessage
-		err = json.NewDecoder(resp.Body).Decode(&res)
-		if err != nil {
-			return nil, err
-		}
-
-		return res, nil
-	}
-
-	return nil, nil
-}
-
-// digitalOceanRecord represents a DigitalOcean DNS record
-type digitalOceanRecord struct {
-	ID   int    `json:"id,omitempty"`
-	Type string `json:"type"`
-	Name string `json:"name"`
-	Data string `json:"data"`
-	TTL  int    `json:"ttl,omitempty"`
+	return records, err
 }
