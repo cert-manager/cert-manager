4c997c297e6b6035e90c8ed975078360f9249799
Max Ehrlich <max.ehr@gmail.com>
2018-08-03 11:53:24 -0400

    Add unit test for acmedns
    Signed-off-by: Max Ehrlich <max.ehr@gmail.com>

--- pkg/issuer/acme/dns/acmedns/acmedns_test.go
+++ pkg/issuer/acme/dns/acmedns/acmedns_test.go
@@ -1,1 +1,33 @@
 package acmedns
+
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+var (
+	acmednsLiveTest     bool
+	acmednsHost         string
+	acmednsAccountsJson []byte
+	acmednsDomain       string
+)
+
+func init() {
+	acmednsHost = os.Getenv("")
+	acmednsAccountsJson = []byte(os.Getenv("AZURE_CLIENT_SECRET"))
+	if len(acmednsHost) > 0 && len(acmednsAccountsJson) > 0 {
+		acmednsLiveTest = true
+	}
+}
+
+func TestLiveAzureDnsPresent(t *testing.T) {
+	if !acmednsLiveTest {
+		t.Skip("skipping live test")
+	}
+	provider, err := NewDNSProviderHostBytes(acmednsHost, acmednsAccountsJson)
+	assert.NoError(t, err)
+
+	err = provider.Present(acmednsDomain, "", "123d==")
+	assert.NoError(t, err)
+}
