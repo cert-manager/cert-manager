90a1881c9c22fea5a22e1d347d3a72484d1c0019
James Munnelly <james.munnelly@jetstack.io>
2018-08-06 16:53:43 +0100

    Update validation to use consts moved into pkg/controller

--- pkg/apis/certmanager/validation/certificate_for_issuer.go
+++ pkg/apis/certmanager/validation/certificate_for_issuer.go
@@ -4,7 +4,7 @@ import (
 	"k8s.io/apimachinery/pkg/util/validation/field"
 
 	"github.com/jetstack/cert-manager/pkg/apis/certmanager/v1alpha1"
-	"github.com/jetstack/cert-manager/pkg/issuer"
+	"github.com/jetstack/cert-manager/pkg/controller"
 )
 
 func ValidateCertificateForIssuer(crt *v1alpha1.Certificate, issuerObj v1alpha1.GenericIssuer) field.ErrorList {
@@ -12,20 +12,20 @@ func ValidateCertificateForIssuer(crt *v1alpha1.Certificate, issuerObj v1alpha1.
 
 	path := field.NewPath("spec")
 
-	issuerType, err := issuer.NameForIssuer(issuerObj)
+	issuerType, err := controller.NameForIssuer(issuerObj)
 	if err != nil {
 		el = append(el, field.Invalid(path, err.Error(), err.Error()))
 		return el
 	}
 
 	switch issuerType {
-	case issuer.IssuerACME:
+	case controller.IssuerACME:
 		el = append(el, ValidateCertificateForACMEIssuer(&crt.Spec, issuerObj.GetSpec(), path)...)
-	case issuer.IssuerCA:
+	case controller.IssuerCA:
 		el = append(el, ValidateCertificateForCAIssuer(&crt.Spec, issuerObj.GetSpec(), path)...)
-	case issuer.IssuerVault:
+	case controller.IssuerVault:
 		el = append(el, ValidateCertificateForVaultIssuer(&crt.Spec, issuerObj.GetSpec(), path)...)
-	case issuer.IssuerSelfSigned:
+	case controller.IssuerSelfSigned:
 		el = append(el, ValidateCertificateForSelfSignedIssuer(&crt.Spec, issuerObj.GetSpec(), path)...)
 	}
 
