48d6a24cc763a9afcde01d92213b51983ca02a88
James Munnelly <james.munnelly@jetstack.io>
2018-08-06 16:53:14 +0100

    Remove dedicated issuer context and move issuer registration into controller pkg

--- pkg/controller/context.go
+++ pkg/controller/context.go
@@ -7,7 +7,6 @@ import (
 
 	clientset "github.com/jetstack/cert-manager/pkg/client/clientset/versioned"
 	informers "github.com/jetstack/cert-manager/pkg/client/informers/externalversions"
-	"github.com/jetstack/cert-manager/pkg/issuer"
 )
 
 // Context contains various types that are used by controller implementations.
@@ -28,14 +27,42 @@ type Context struct {
 	// SharedInformerFactory can be used to obtain shared SharedIndexInformer
 	// instances
 	SharedInformerFactory informers.SharedInformerFactory
-	// IssuerFactory is a factory that can be used to obtain issuer.Interface
-	// instances
-	IssuerFactory issuer.Factory
 
+	IssuerOptions
+	ACMEOptions
+	IngressShimOptions
+}
+
+func (c *Context) IssuerFactory() IssuerFactory {
+	return NewIssuerFactory(c)
+}
+
+type IssuerOptions struct {
 	// ClusterResourceNamespace is the namespace to store resources created by
 	// non-namespaced resources (e.g. ClusterIssuer) in.
 	ClusterResourceNamespace string
 
+	// ClusterIssuerAmbientCredentials controls whether a cluster issuer should
+	// pick up ambient credentials, such as those from metadata services, to
+	// construct clients.
+	ClusterIssuerAmbientCredentials bool
+
+	// IssuerAmbientCredentials controls whether an issuer should pick up ambient
+	// credentials, such as those from metadata services, to construct clients.
+	IssuerAmbientCredentials bool
+}
+
+type ACMEOptions struct {
+	// ACMEHTTP01SolverImage is the image to use for solving ACME HTTP01
+	// challenges
+	HTTP01SolverImage string
+
+	// DNS01Nameservers is a list of nameservers to use when performing self-checks
+	// for ACME DNS01 validations.
+	DNS01Nameservers []string
+}
+
+type IngressShimOptions struct {
 	// Default issuer/certificates details consumed by ingress-shim
 	DefaultIssuerName                  string
 	DefaultIssuerKind                  string
