27c9a07f3d7ec0718dda998e3102173eab6dea37
James Munnelly <james.munnelly@jetstack.io>
2018-08-13 15:14:42 +0100

    Update license header and add header to every file
    Signed-off-by: James Munnelly <james.munnelly@jetstack.io>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Copyright 2018 The Jetstack cert-manager contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # The only argument this script should ever be called with is '--verify-only'
 
 set -o errexit
@@ -13,4 +27,4 @@ CODEGEN_PKG=${CODEGEN_PKG:-$(CDPATH='' cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8
   github.com/jetstack/cert-manager/pkg/client github.com/jetstack/cert-manager/pkg/apis \
   certmanager:v1alpha1 \
   --output-base "${GOPATH}/src/" \
-  --go-header-file "${SCRIPT_ROOT}/hack/boilerplate.go.txt"
+  --go-header-file "${SCRIPT_ROOT}/hack/boilerplate/boilerplate.go.txt"
