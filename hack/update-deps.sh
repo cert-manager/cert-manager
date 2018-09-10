bdd5eb13f2f50c8f89c32a3e714fcd80346c4639
James Munnelly <james@munnelly.eu>
2018-09-10 22:57:50 +0100

    Add update-deps bazel targets
    Signed-off-by: James Munnelly <james@munnelly.eu>

--- hack/update-deps.sh
+++ hack/update-deps.sh
@@ -18,9 +18,12 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-SCRIPT_ROOT=$(dirname "${BASH_SOURCE}")
-REPO_ROOT="${SCRIPT_ROOT}/.."
-pushd "${REPO_ROOT}"
+# This script should be run via `bazel run //hack:update-deps`
+REPO_ROOT=${BUILD_WORKSPACE_DIRECTORY:-"$(cd "$(dirname "$0")" && pwd -P)"/..}
+runfiles="$(pwd)"
+export PATH="${runfiles}/hack/bin:${PATH}"
+cd "${REPO_ROOT}"
+
 echo "+++ Running dep ensure"
 dep ensure -v "$@"
 echo "+++ Cleaning up erroneous vendored testdata symlinks"
@@ -32,4 +35,10 @@ rm -Rf vendor/github.com/prometheus/procfs/fixtures \
        vendor/github.com/coreos/etcd/cmd/functional \
        vendor/github.com/coreos/etcd/cmd/tools \
        vendor/github.com/coreos/etcd/cmd/etcd
-popd
+
+echo "+++ Deleting bazel related data in vendor/"
+find vendor/ -type f \( -name BUILD -o -name BUILD.bazel -o -name WORKSPACE \) \
+  -exec rm -f {} \;
+
+touch vendor/BUILD.bazel
+hack/update-bazel.sh
