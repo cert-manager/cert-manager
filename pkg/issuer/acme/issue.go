511650ca8272f6157cad93aaf7283997d3e13d1f
Max Ehrlich <max.ehr@gmail.com>
2018-09-13 17:01:17 -0400

    ACME issuers currently will not support getting the CA certificate
    Signed-off-by: Max Ehrlich <max.ehr@gmail.com>

--- pkg/issuer/acme/issue.go
+++ pkg/issuer/acme/issue.go
@@ -205,10 +205,10 @@ func (a *Acme) obtainCertificate(ctx context.Context, crt *v1alpha1.Certificate)
 	return keyPem, certBuffer.Bytes(), nil
 }
 
-func (a *Acme) Issue(ctx context.Context, crt *v1alpha1.Certificate) ([]byte, []byte, error) {
+func (a *Acme) Issue(ctx context.Context, crt *v1alpha1.Certificate) ([]byte, []byte, []byte, error) {
 	key, cert, err := a.obtainCertificate(ctx, crt)
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, nil, err
 	}
-	return key, cert, err
+	return key, cert, nil, err
 }
--- pkg/issuer/acme/renew.go
+++ pkg/issuer/acme/renew.go
@@ -22,10 +22,10 @@ import (
 	"github.com/jetstack/cert-manager/pkg/apis/certmanager/v1alpha1"
 )
 
-func (a *Acme) Renew(ctx context.Context, crt *v1alpha1.Certificate) ([]byte, []byte, error) {
+func (a *Acme) Renew(ctx context.Context, crt *v1alpha1.Certificate) ([]byte, []byte, []byte, error) {
 	key, cert, err := a.obtainCertificate(ctx, crt)
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, nil, err
 	}
-	return key, cert, err
+	return key, cert, nil, err
 }
