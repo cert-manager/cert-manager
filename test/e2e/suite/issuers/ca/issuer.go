24e54d0a587824bd2275037a3a61bb58af569c8c
Max Ehrlich <max.ehr@gmail.com>
2018-09-16 13:03:30 -0400

    Update e2e tests to work with new validation
    Signed-off-by: Max Ehrlich <max.ehr@gmail.com>

--- test/e2e/suite/issuers/ca/issuer.go
+++ test/e2e/suite/issuers/ca/issuer.go
@@ -17,10 +17,9 @@ limitations under the License.
 package ca
 
 import (
-	"time"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"time"
 
 	"github.com/jetstack/cert-manager/pkg/apis/certmanager/v1alpha1"
 	"github.com/jetstack/cert-manager/test/e2e/framework"
@@ -58,48 +57,17 @@ var _ = framework.CertManagerDescribe("CA Issuer", func() {
 		Expect(err).NotTo(HaveOccurred())
 	})
 
-	cases := []struct {
-		inputDuration    time.Duration
-		inputRenewBefore time.Duration
-		label            string
-		status           v1alpha1.ConditionStatus
-	}{
-		{
-			inputDuration:    0,
-			inputRenewBefore: time.Hour * 24 * 365 * 10,
-			label:            "should fail when renewBefore is bigger than the duration",
-			status:           v1alpha1.ConditionStatus(v1alpha1.ConditionFalse),
-		},
-		{
-			inputDuration:    0,
-			inputRenewBefore: time.Second,
-			label:            "should fail when renewBefore is less than the minimum permitted value",
-			status:           v1alpha1.ConditionStatus(v1alpha1.ConditionFalse),
-		},
-		{
-			inputDuration:    time.Second,
-			inputRenewBefore: 0,
-			label:            "should fail when duration is less than the minimum permitted value",
-			status:           v1alpha1.ConditionStatus(v1alpha1.ConditionFalse),
-		},
-	}
-	for _, v := range cases {
-		v := v
-		It(v.label, func() {
-			issuerName := "test-issuer-duration"
-			By("Creating an Issuer")
-			_, err := f.CertManagerClientSet.CertmanagerV1alpha1().Issuers(f.Namespace.Name).Create(util.NewCertManagerCAIssuer(issuerName, secretName, v.inputDuration, v.inputRenewBefore))
-			Expect(err).NotTo(HaveOccurred())
-			By("Waiting for Issuer to become Ready")
-			err = util.WaitForIssuerCondition(f.CertManagerClientSet.CertmanagerV1alpha1().Issuers(f.Namespace.Name),
-				issuerName,
-				v1alpha1.IssuerCondition{
-					Type:   v1alpha1.IssuerConditionReady,
-					Status: v.status,
-				})
-			Expect(err).NotTo(HaveOccurred())
-			By("Cleaning up")
-			f.CertManagerClientSet.CertmanagerV1alpha1().Issuers(f.Namespace.Name).Delete(issuerName, nil)
-		})
-	}
+	It("should create a CA issuer with a custom duration and renewBefore", func() {
+		By("Creating an Issuer")
+		_, err := f.CertManagerClientSet.CertmanagerV1alpha1().Issuers(f.Namespace.Name).Create(util.NewCertManagerCAIssuer(issuerName, secretName, time.Hour*24*365, time.Hour*24*180))
+		Expect(err).NotTo(HaveOccurred())
+		By("Waiting for Issuer to become Ready")
+		err = util.WaitForIssuerCondition(f.CertManagerClientSet.CertmanagerV1alpha1().Issuers(f.Namespace.Name),
+			issuerName,
+			v1alpha1.IssuerCondition{
+				Type:   v1alpha1.IssuerConditionReady,
+				Status: v1alpha1.ConditionTrue,
+			})
+		Expect(err).NotTo(HaveOccurred())
+	})
 })
