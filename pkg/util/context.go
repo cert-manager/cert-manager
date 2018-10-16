16db6f3fe070d3a6fb44f0dd2b1c28953d0cf132
James Munnelly <james@munnelly.eu>
2018-10-16 10:15:45 +0100

    Add comments to pkg/util functions
    Signed-off-by: James Munnelly <james@munnelly.eu>

--- pkg/util/context.go
+++ pkg/util/context.go
@@ -20,6 +20,9 @@ import (
 	"context"
 )
 
+// ContextWithStopCh will wrap a context with a stop channel.
+// When the provided stopCh closes, the cancel() will be called on the context.
+// This provides a convinient way to represent a stop channel as a context.
 func ContextWithStopCh(ctx context.Context, stopCh <-chan struct{}) context.Context {
 	ctx, cancel := context.WithCancel(ctx)
 	go func() {
