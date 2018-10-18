459613880a7cd3bea4c7444cc9c6bcb3ad874e3b
James Munnelly <james@munnelly.eu>
2018-10-18 13:26:13 +0100

    Update boilerplate headers
    Signed-off-by: James Munnelly <james@munnelly.eu>

--- test/e2e/suite/issuers/acme/dnsproviders/cloudflare.go
+++ test/e2e/suite/issuers/acme/dnsproviders/cloudflare.go
@@ -1,5 +1,19 @@
-// Package base implements a basis for plugins that need to use the Kubernetes
-// API to build upon.
+/*
+Copyright 2018 The Jetstack cert-manager contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package dnsproviders
 
 import (
@@ -26,7 +40,6 @@ type Cloudflare struct {
 	createdSecret *corev1.Secret
 }
 
-// Details return the details about the certmanager instance deployed
 type Details struct {
 	// Domain is a domain that can be validated using these credentials
 	Domain string
