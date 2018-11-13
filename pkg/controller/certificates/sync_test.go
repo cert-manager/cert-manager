cf7d260210e69d4ff0cccd2296d12885acb76900
Max Ehrlich <max.ehr@gmail.com>
2018-11-12 19:01:40 -0500

    Fix sync unit test to match e2e test format
    Signed-off-by: Max Ehrlich <max.ehr@gmail.com>

--- pkg/controller/certificates/sync_test.go
+++ pkg/controller/certificates/sync_test.go
@@ -34,64 +34,64 @@ func TestCalculateTimeBeforeExpiry(t *testing.T) {
 		desc           string
 		notBefore      time.Time
 		notAfter       time.Time
-		duration       time.Duration
-		renewBefore    time.Duration
+		duration       *metav1.Duration
+		renewBefore    *metav1.Duration
 		expectedExpiry time.Duration
 	}{
 		{
 			desc:           "generate an event if certificate duration is lower than requested duration",
 			notBefore:      now(),
 			notAfter:       now().Add(time.Hour * 24 * 90),
-			duration:       time.Hour * 24 * 120,
-			renewBefore:    0,
+			duration:       &metav1.Duration{time.Hour * 24 * 120},
+			renewBefore:    nil,
 			expectedExpiry: time.Hour * 24 * 60,
 		},
 		{
 			desc:           "default expiry to 30 days",
 			notBefore:      now(),
 			notAfter:       now().Add(time.Hour * 24 * 120),
-			duration:       0,
-			renewBefore:    0,
+			duration:       nil,
+			renewBefore:    nil,
 			expectedExpiry: (time.Hour * 24 * 120) - (time.Hour * 24 * 30),
 		},
 		{
 			desc:           "default expiry to 2/3 of total duration if duration < 30 days",
 			notBefore:      now(),
 			notAfter:       now().Add(time.Hour * 24 * 20),
-			duration:       0,
-			renewBefore:    0,
+			duration:       nil,
+			renewBefore:    nil,
 			expectedExpiry: time.Hour * 24 * 20 * 2 / 3,
 		},
 		{
 			desc:           "expiry of 2/3 of certificate duration when duration < 30 minutes",
 			notBefore:      now(),
 			notAfter:       now().Add(time.Hour),
-			duration:       time.Hour,
-			renewBefore:    time.Hour / 3,
+			duration:       &metav1.Duration{time.Hour},
+			renewBefore:    &metav1.Duration{time.Hour / 3},
 			expectedExpiry: time.Hour * 2 / 3,
 		},
 		{
 			desc:           "expiry of 60 days of certificate duration",
 			notBefore:      now(),
 			notAfter:       now().Add(time.Hour * 24 * 365),
-			duration:       time.Hour * 24 * 365,
-			renewBefore:    time.Hour * 24 * 60,
+			duration:       &metav1.Duration{time.Hour * 24 * 365},
+			renewBefore:    &metav1.Duration{time.Hour * 24 * 60},
 			expectedExpiry: (time.Hour * 24 * 365) - (time.Hour * 24 * 60),
 		},
 		{
 			desc:           "expiry of 2/3 of certificate duration when renewBefore greater than certificate duration",
 			notBefore:      now(),
 			notAfter:       now().Add(time.Hour * 24 * 35),
-			duration:       time.Hour * 24 * 35,
-			renewBefore:    time.Hour * 24 * 40,
+			duration:       &metav1.Duration{time.Hour * 24 * 35},
+			renewBefore:    &metav1.Duration{time.Hour * 24 * 40},
 			expectedExpiry: time.Hour * 24 * 35 * 2 / 3,
 		},
 	}
 	for k, v := range tests {
 		cert := &v1alpha1.Certificate{
 			Spec: v1alpha1.CertificateSpec{
-				Duration:    &metav1.Duration{v.duration},
-				RenewBefore: &metav1.Duration{v.renewBefore},
+				Duration:    v.duration,
+				RenewBefore: v.renewBefore,
 			},
 		}
 		x509Cert := &x509.Certificate{NotBefore: v.notBefore, NotAfter: v.notAfter}
