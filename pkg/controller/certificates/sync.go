acc0fa887b99a173106e5e0128f99ee47accbd27
Gus Parvin <gparvin@us.ibm.com>
2018-11-15 14:30:31 +0000

    set the NotAfter time as soon as the cert is parsed successfully
    Signed-off-by: Gus Parvin <gparvin@us.ibm.com>

--- pkg/controller/certificates/sync.go
+++ pkg/controller/certificates/sync.go
@@ -156,6 +156,9 @@ func (c *Controller) Sync(ctx context.Context, crt *v1alpha1.Certificate) (reque
 		return false, err
 	}
 
+	metaNotAfter := metav1.NewTime(cert.NotAfter)
+	crtCopy.Status.NotAfter = &metaNotAfter
+
 	// begin checking if the TLS certificate is valid/needs a re-issue or renew
 
 	// check if the private key is the corresponding pair to the certificate
@@ -178,9 +181,6 @@ func (c *Controller) Sync(ctx context.Context, crt *v1alpha1.Certificate) (reque
 	if !util.EqualUnsorted(cert.DNSNames, expectedDNSNames) {
 		return c.issue(ctx, i, crtCopy)
 	}
-
-	metaNotAfter := metav1.NewTime(cert.NotAfter)
-	crtCopy.Status.NotAfter = &metaNotAfter
 
 	// check if the certificate needs renewal
 	needsRenew := c.Context.IssuerOptions.CertificateNeedsRenew(cert)
