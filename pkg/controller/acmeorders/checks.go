79ee839c8a85a397e8d28dabf2ac2c6fdb2bca1d
James Munnelly <james@munnelly.eu>
2018-11-28 19:16:00 +0000

    Resync order resources when their issuers change
    Signed-off-by: James Munnelly <james@munnelly.eu>
    Signed-off-by: Laurent Rolaz <lrolaz@pictet.com>

--- pkg/controller/acmeorders/checks.go
+++ pkg/controller/acmeorders/checks.go
@@ -16,4 +16,60 @@ limitations under the License.
 
 package acmeorders
 
-// no checks for the acme orders controller yet
+import (
+	"fmt"
+
+	cmapi "github.com/jetstack/cert-manager/pkg/apis/certmanager/v1alpha1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/util/runtime"
+)
+
+func (c *Controller) handleGenericIssuer(obj interface{}) {
+	iss, ok := obj.(cmapi.GenericIssuer)
+	if !ok {
+		runtime.HandleError(fmt.Errorf("Object does not implement GenericIssuer %#v", obj))
+		return
+	}
+
+	certs, err := c.ordersForGenericIssuer(iss)
+	if err != nil {
+		runtime.HandleError(fmt.Errorf("Error looking up Orders observing Issuer/ClusterIssuer: %s/%s", iss.GetObjectMeta().Namespace, iss.GetObjectMeta().Name))
+		return
+	}
+	for _, crt := range certs {
+		key, err := keyFunc(crt)
+		if err != nil {
+			runtime.HandleError(err)
+			continue
+		}
+		c.queue.Add(key)
+	}
+}
+
+func (c *Controller) ordersForGenericIssuer(iss cmapi.GenericIssuer) ([]*cmapi.Order, error) {
+	orders, err := c.orderLister.List(labels.NewSelector())
+
+	if err != nil {
+		return nil, fmt.Errorf("error listing certificiates: %s", err.Error())
+	}
+
+	_, isClusterIssuer := iss.(*cmapi.ClusterIssuer)
+
+	var affected []*cmapi.Order
+	for _, o := range orders {
+		if isClusterIssuer && o.Spec.IssuerRef.Kind != cmapi.ClusterIssuerKind {
+			continue
+		}
+		if !isClusterIssuer {
+			if o.Namespace != iss.GetObjectMeta().Namespace {
+				continue
+			}
+		}
+		if o.Spec.IssuerRef.Name != iss.GetObjectMeta().Name {
+			continue
+		}
+		affected = append(affected, o)
+	}
+
+	return affected, nil
+}
