0fcc0c666c1d4600e29d3d3abd0471319e2d4324
James Munnelly <james@munnelly.eu>
2019-01-07 10:52:25 +0000

    Update copyright header year
    Signed-off-by: James Munnelly <james@munnelly.eu>

--- hack/verify-deps.sh
+++ hack/verify-deps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright 2018 The Jetstack cert-manager contributors.
+# Copyright 2019 The Jetstack cert-manager contributors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
