379aacf822faf33b1897a2b67677c0832f94f85e
Daniel Morsing <dmo@jetstack.io>
2019-02-04 10:59:01 +0000

    skip test until nginx-ingress is fixed
    Signed-off-by: Daniel Morsing <dmo@jetstack.io>

--- test/e2e/suite/issuers/acme/certificate/http01.go
+++ test/e2e/suite/issuers/acme/certificate/http01.go
@@ -225,6 +225,8 @@ var _ = framework.CertManagerDescribe("ACME Certificate (HTTP01)", func() {
 	})
 
 	It("should obtain a signed certificate with a single CN from the ACME server when redirected", func() {
+		Skip("nginx-ingress bug stops pebble from getting a certificate https://github.com/kubernetes/ingress-nginx/issues/3192")
+
 		certClient := f.CertManagerClientSet.CertmanagerV1alpha1().Certificates(f.Namespace.Name)
 		secretClient := f.KubeClientSet.CoreV1().Secrets(f.Namespace.Name)
 
