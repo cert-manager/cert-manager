942d6491d615043176240282dba782f5e880a885
James Munnelly <james@munnelly.eu>
2019-04-18 14:11:35 +0100

    Pass StopCh and RESTClient to controller context
    Signed-off-by: James Munnelly <james@munnelly.eu>

--- cmd/controller/app/controller.go
+++ cmd/controller/app/controller.go
@@ -56,7 +56,7 @@ func Run(opts *options.ControllerOptions, stopCh <-chan struct{}) {
 	rootCtx = logf.NewContext(rootCtx, nil, "controller")
 	log := logf.FromContext(rootCtx)
 
-	ctx, kubeCfg, err := buildControllerContext(rootCtx, opts)
+	ctx, kubeCfg, err := buildControllerContext(rootCtx, stopCh, opts)
 
 	if err != nil {
 		log.Error(err, "error building controller context", "options", opts)
@@ -131,9 +131,8 @@ func Run(opts *options.ControllerOptions, stopCh <-chan struct{}) {
 	panic("unreachable")
 }
 
-func buildControllerContext(ctx context.Context, opts *options.ControllerOptions) (*controller.Context, *rest.Config, error) {
+func buildControllerContext(ctx context.Context, stopCh <-chan struct{}, opts *options.ControllerOptions) (*controller.Context, *rest.Config, error) {
 	log := logf.FromContext(ctx, "build-context")
-
 	// Load the users Kubernetes config
 	kubeCfg, err := kube.KubeConfig(opts.APIServerHost)
 	if err != nil {
@@ -192,6 +191,8 @@ func buildControllerContext(ctx context.Context, opts *options.ControllerOptions
 	kubeSharedInformerFactory := kubeinformers.NewFilteredSharedInformerFactory(cl, time.Second*30, opts.Namespace, nil)
 	return &controller.Context{
 		RootContext:               ctx,
+		StopCh:                    stopCh,
+		RESTConfig:                kubeCfg,
 		Client:                    cl,
 		CMClient:                  intcl,
 		Recorder:                  recorder,
