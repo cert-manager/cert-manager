fcc996eec954eee2d4f3b81a98691f94c2a369c1
JoshVanL <vleeuwenjoshua@gmail.com>
2019-06-17 11:16:26 +0100

    Create CertificateRequest resource Names based on Certificate Spec hash
    Signed-off-by: JoshVanL <vleeuwenjoshua@gmail.com>

--- pkg/controller/certificates/certificaterequest.go
+++ pkg/controller/certificates/certificaterequest.go
@@ -19,15 +19,14 @@ import (
 	"bytes"
 	"context"
 	"crypto"
+	"encoding/json"
 	"encoding/pem"
 	"fmt"
-	"strings"
+	"hash/fnv"
 
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/labels"
-	"k8s.io/apimachinery/pkg/selection"
-	utilerrors "k8s.io/apimachinery/pkg/util/errors"
 
 	apiutil "github.com/jetstack/cert-manager/pkg/api/util"
 	"github.com/jetstack/cert-manager/pkg/apis/certmanager/v1alpha1"
@@ -43,7 +42,6 @@ const (
 // and private key will be stored in the named secret
 func (c *Controller) issue(ctx context.Context, issuer v1alpha1.GenericIssuer, key crypto.Signer, crt *v1alpha1.Certificate) error {
 	log := logf.FromContext(ctx)
-	dbg := log.V(logf.DebugLevel)
 
 	csr, err := pki.GenerateCSR(issuer, crt)
 	if err != nil {
@@ -67,110 +65,66 @@ func (c *Controller) issue(ctx context.Context, issuer v1alpha1.GenericIssuer, k
 		Type: "CERTIFICATE REQUEST", Bytes: csrBytes,
 	})
 
-	crs, err := c.listCertificateRequestsForCertificate(crt)
-	if err != nil {
+	cr, err := c.getCertificateRequestForCertificate(crt, csrPEM)
+	if err != nil && !apierrors.IsNotFound(err) {
+		log.WithValues(
+			logf.RelatedResourceNamespaceKey, cr.Namespace,
+			logf.RelatedResourceNameKey, cr.Name,
+			logf.RelatedResourceKindKey, v1alpha1.CertificateRequestKind,
+		).Error(err, "error getting existing CertificateRequest resource")
 		return err
 	}
 
 	// create CertificateRequest
-	if len(crs) == 0 {
+	if cr == nil {
 		// return here and wait till next sync to catch CertificateRequest update
+		log.Info("creating CertificateRequest to issue certificate")
 		return c.createCertificateRequest(crt, csrPEM)
 	}
 
-	// Loop through CertificateRequests and if any don't match our current
-	// Certificate, delete
-	var delErrs []error
-	for i := 0; i < len(crs); i++ {
-
-		matches, matchErrs := c.certificateRequestMatchesCertificateSpec(crt, csrPEM, crs[i])
-		if !matches {
-
-			dbg.Info(fmt.Sprintf("certificate request spec does not match certificate, deleting %s/%s",
-				crs[i].Namespace, crs[i].Name), "diff", strings.Join(matchErrs, ", "))
-
-			err = c.deleteCertificateRequests(crs[i])
-			if err != nil {
-				delErrs = append(delErrs, err)
-			}
-
-			crs = append(crs[:i], crs[i+1:]...)
-			i -= 1
-		}
+	ready := apiutil.CertificateRequestHasCondition(cr, v1alpha1.CertificateRequestCondition{
+		Type:   v1alpha1.CertificateRequestConditionReady,
+		Status: v1alpha1.ConditionTrue,
+	})
+	if !ready {
+		// CertificateRequest is not ready yet so wait for next sync
+		return nil
 	}
 
-	if len(delErrs) > 0 {
-		return utilerrors.NewAggregate(delErrs)
+	keyPEM, err := pki.EncodePrivateKey(key)
+	if err != nil {
+		return err
 	}
 
-	// no CertificateRequests left
-	if len(crs) == 0 {
-		return c.createCertificateRequest(crt, csrPEM)
+	// Update referenced secret with ready certificate
+	if _, err := c.updateSecret(ctx, crt, crt.Namespace, cr.Status.Certificate, keyPEM, cr.Status.CA); err != nil {
+		s := messageErrorSavingCertificate + err.Error()
+		log.Error(err, "error saving certificate")
+		c.Recorder.Event(crt, corev1.EventTypeWarning, errorSavingCertificate, s)
+		return err
 	}
 
-	// We loop through all CertificateRequests to find one that's ready.
-	// If we don't find one then we back off.
-	// If we do find one that's valid, get the cert and delete.
-	for _, cr := range crs {
-		ready := apiutil.CertificateRequestHasCondition(cr, v1alpha1.CertificateRequestCondition{
-			Type:   v1alpha1.CertificateRequestConditionReady,
-			Status: v1alpha1.ConditionTrue,
-		})
-
-		if !ready {
-			continue
-		}
-
-		keyPem, err := pki.EncodePrivateKey(key)
-		if err != nil {
-			return err
-		}
-
-		if _, err := c.updateSecret(ctx, crt, crt.Namespace, cr.Status.Certificate, keyPem, cr.Status.CA); err != nil {
-			s := messageErrorSavingCertificate + err.Error()
-			log.Error(err, "error saving certificate")
-			c.Recorder.Event(crt, corev1.EventTypeWarning, errorSavingCertificate, s)
-			return err
-		}
-
-		certx509, err := pki.DecodeX509CertificateBytes(cr.Status.Certificate)
-		if err != nil {
-			return err
-		}
-
-		c.setCertificateStatus(crt, key, certx509)
-
-		c.Recorder.Event(crt, corev1.EventTypeNormal, successCertificateIssued, "Certificate issued successfully")
-		// as we have just written a certificate, we should schedule it for renewal
-		c.scheduleRenewal(ctx, crt)
-
-		return c.deleteCertificateRequests(crs...)
+	certx509, err := pki.DecodeX509CertificateBytes(cr.Status.Certificate)
+	if err != nil {
+		return err
 	}
 
-	// No CertificateRequests are ready yet so we wait till the next sync
+	c.setCertificateStatus(crt, key, certx509)
+
+	c.Recorder.Event(crt, corev1.EventTypeNormal, successCertificateIssued, "Certificate issued successfully")
+	// as we have just written a certificate, we should schedule it for renewal
+	c.scheduleRenewal(ctx, crt)
 
 	return nil
 }
 
-func (c *Controller) deleteCertificateRequests(crs ...*v1alpha1.CertificateRequest) error {
-	var errs []error
-
-	for _, cr := range crs {
-		err := c.CMClient.CertmanagerV1alpha1().CertificateRequests(cr.Namespace).
-			Delete(cr.Name, nil)
-
-		if err != nil {
-			errs = append(errs, fmt.Errorf("failed to delete certificate request %s/%s",
-				cr.Namespace, cr.Name))
-		}
+func (c *Controller) createCertificateRequest(crt *v1alpha1.Certificate, csrPEM []byte) error {
+	cr, err := buildCertificateRequest(crt, csrPEM)
+	if err != nil {
+		return err
 	}
 
-	return utilerrors.NewAggregate(errs)
-}
-
-func (c *Controller) createCertificateRequest(crt *v1alpha1.Certificate, csrPEM []byte) error {
-	_, err := c.CMClient.CertmanagerV1alpha1().CertificateRequests(crt.Namespace).Create(
-		buildCertificateRequest(csrPEM, crt))
+	_, err = c.CMClient.CertmanagerV1alpha1().CertificateRequests(crt.Namespace).Create(cr)
 	if err != nil {
 		return err
 	}
@@ -210,12 +164,17 @@ func (c *Controller) certificateRequestMatchesCertificateSpec(cert *v1alpha1.Cer
 	return len(errs) == 0, errs
 }
 
-func buildCertificateRequest(csr []byte, crt *v1alpha1.Certificate) *v1alpha1.CertificateRequest {
+func buildCertificateRequest(crt *v1alpha1.Certificate, csr []byte) (*v1alpha1.CertificateRequest, error) {
+	hash, err := hashCertificate(crt.Spec)
+	if err != nil {
+		return nil, err
+	}
+
 	return &v1alpha1.CertificateRequest{
 		ObjectMeta: metav1.ObjectMeta{
-			GenerateName: crt.Name + "-",
-			Namespace:    crt.Namespace,
-			Labels:       certificateRequestLabelsForCertificate(crt),
+			Name:      fmt.Sprintf("%s-%d", crt.Name, hash),
+			Namespace: crt.Namespace,
+			Labels:    certificateRequestLabelsForCertificate(crt),
 		},
 		Spec: v1alpha1.CertificateRequestSpec{
 			CSRPEM:    csr,
@@ -223,34 +182,37 @@ func buildCertificateRequest(csr []byte, crt *v1alpha1.Certificate) *v1alpha1.Ce
 			IsCA:      crt.Spec.IsCA,
 			IssuerRef: crt.Spec.IssuerRef,
 		},
-	}
+	}, nil
 }
 
-func (c *Controller) listCertificateRequestsForCertificate(crt *v1alpha1.Certificate) ([]*v1alpha1.CertificateRequest, error) {
-	// create a selector that we can use to find all existing CertificateRequests for the Certificate
-	sel, err := certificateRequestSelectorForCertificate(crt)
+func hashCertificate(crtSpec v1alpha1.CertificateSpec) (uint32, error) {
+	crtSpecBytes, err := json.Marshal(crtSpec)
+	if err != nil {
+		return 0, err
+	}
+
+	hashF := fnv.New32()
+	_, err = hashF.Write(crtSpecBytes)
+	if err != nil {
+		return 0, err
+	}
+
+	return hashF.Sum32(), nil
+}
+
+func (c *Controller) getCertificateRequestForCertificate(crt *v1alpha1.Certificate, csr []byte) (*v1alpha1.CertificateRequest, error) {
+	expectedCertificateRequest, err := buildCertificateRequest(crt, csr)
 	if err != nil {
 		return nil, err
 	}
 
-	// get the list of exising certificate requests for this certificate
-	return c.certificateRequestLister.CertificateRequests(crt.Namespace).List(sel)
-}
-
-// challengeSelectorForCertificateRequest will construct a labels.Selector that
-// can be used to find CertificateRequests associated with the given
-// Certificate.
-func certificateRequestSelectorForCertificate(c *v1alpha1.Certificate) (labels.Selector, error) {
-	lbls := certificateRequestLabelsForCertificate(c)
-	var reqs []labels.Requirement
-	for k, v := range lbls {
-		req, err := labels.NewRequirement(k, selection.Equals, []string{v})
-		if err != nil {
-			return nil, err
-		}
-		reqs = append(reqs, *req)
+	cr, err := c.certificateRequestLister.CertificateRequests(expectedCertificateRequest.Namespace).
+		Get(expectedCertificateRequest.Name)
+	if err != nil {
+		return nil, err
 	}
-	return labels.NewSelector().Add(reqs...), nil
+
+	return cr, nil
 }
 
 func certificateRequestLabelsForCertificate(c *v1alpha1.Certificate) map[string]string {
