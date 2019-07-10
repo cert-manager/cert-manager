b7172920906d885385fc9a8bdd77cbefdde03503
Michael Tsang <michael.tsang@jetstack.io>
2019-07-10 17:09:26 +0100

    Rewrite if/else for nil selector
    Signed-off-by: Michael Tsang <michael.tsang@jetstack.io>

--- pkg/controller/acmeorders/sync.go
+++ pkg/controller/acmeorders/sync.go
@@ -453,13 +453,13 @@ func challengeSpecForAuthorization(ctx context.Context, cl acmecl.Interface, iss
 		}
 
 		if cfg.Selector == nil {
-			if selectedSolver == nil {
-				dbg.Info("selecting solver due to nil selector and no previously selected solver")
-				selectedSolver = cfg.DeepCopy()
-				selectedChallenge = acmech
-			} else {
+			if selectedSolver != nil {
 				dbg.Info("not selecting solver as previously selected solver has a just as or more specific selector")
+				continue
 			}
+			dbg.Info("selecting solver due to match all selector and no previously selected solver")
+			selectedSolver = cfg.DeepCopy()
+			selectedChallenge = acmech
 			continue
 		}
 
