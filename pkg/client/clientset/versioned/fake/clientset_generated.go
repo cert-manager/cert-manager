74c9c41e787ba2b6b27da199bb81e7b6c320616b
James Munnelly <james@munnelly.eu>
2019-07-19 23:16:36 +0100

    Regenerate client
    Signed-off-by: James Munnelly <james@munnelly.eu>

--- pkg/client/clientset/versioned/fake/clientset_generated.go
+++ pkg/client/clientset/versioned/fake/clientset_generated.go
@@ -41,7 +41,7 @@ func NewSimpleClientset(objects ...runtime.Object) *Clientset {
 		}
 	}
 
-	cs := &Clientset{}
+	cs := &Clientset{tracker: o}
 	cs.discovery = &fakediscovery.FakeDiscovery{Fake: &cs.Fake}
 	cs.AddReactor("*", "*", testing.ObjectReaction(o))
 	cs.AddWatchReactor("*", func(action testing.Action) (handled bool, ret watch.Interface, err error) {
@@ -63,20 +63,20 @@ func NewSimpleClientset(objects ...runtime.Object) *Clientset {
 type Clientset struct {
 	testing.Fake
 	discovery *fakediscovery.FakeDiscovery
+	tracker   testing.ObjectTracker
 }
 
 func (c *Clientset) Discovery() discovery.DiscoveryInterface {
 	return c.discovery
 }
 
+func (c *Clientset) Tracker() testing.ObjectTracker {
+	return c.tracker
+}
+
 var _ clientset.Interface = &Clientset{}
 
 // CertmanagerV1alpha1 retrieves the CertmanagerV1alpha1Client
 func (c *Clientset) CertmanagerV1alpha1() certmanagerv1alpha1.CertmanagerV1alpha1Interface {
-	return &fakecertmanagerv1alpha1.FakeCertmanagerV1alpha1{Fake: &c.Fake}
-}
-
-// Certmanager retrieves the CertmanagerV1alpha1Client
-func (c *Clientset) Certmanager() certmanagerv1alpha1.CertmanagerV1alpha1Interface {
 	return &fakecertmanagerv1alpha1.FakeCertmanagerV1alpha1{Fake: &c.Fake}
 }
