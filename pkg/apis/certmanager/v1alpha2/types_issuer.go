06ce5061bac9bdc528c3cecec4b8e971ffdd4664
JoshVanL <vleeuwenjoshua@gmail.com>
2019-11-11 12:25:37 +0000

    Set printer columns by changing crds in deploy
    Signed-off-by: JoshVanL <vleeuwenjoshua@gmail.com>

--- pkg/apis/certmanager/v1alpha2/types_issuer.go
+++ pkg/apis/certmanager/v1alpha2/types_issuer.go
@@ -28,9 +28,6 @@ import (
 // +k8s:openapi-gen=true
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// +kubebuilder:printcolumn:name="Ready",type="string",JSONPath=".status.conditions[?(@.type==\"Ready\")].status",description=""
-// +kubebuilder:printcolumn:name="Status",type="string",JSONPath=".status.conditions[?(@.type==\"Ready\")].message",description=""
-// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp",description="CreationTimestamp is a timestamp representing the server time when this object was created. It is not guaranteed to be set in happens-before order across separate operations. Clients may not set this value. It is represented in RFC3339 form and is in UTC."
 // +kubebuilder:subresource:status
 // +kubebuilder:resource:path=clusterissuers,scope=Cluster
 type ClusterIssuer struct {
