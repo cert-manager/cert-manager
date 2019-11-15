a9006790b7e1a2db87f80acfe9c307671e4359d6
James Munnelly <james@munnelly.eu>
2019-11-15 00:08:54 +0000

    Rewrite original rst files with redirect text
    Signed-off-by: James Munnelly <james@munnelly.eu>

--- docs/_ext/externalredirect.py
+++ docs/_ext/externalredirect.py
@@ -16,6 +16,13 @@ TEMPLATE = """<html>
 </html>
 """
 
+SRC_TEMPLATE = """==========
+File moved
+==========
+
+This document has moved to %s.
+This placeholder file will be removed in a later release.
+"""
 
 def generate_external_redirects(app, exception):
     logger = logging.getLogger(__name__)
@@ -48,7 +55,7 @@ def generate_external_redirects(app, exception):
     with open(path) as redirects:
         for line in redirects.readlines():
             from_path, to_url = line.rstrip().split(' ')
-
+            orig_from_path = from_path
             logger.info("Redirecting '%s' to '%s'" % (from_path, to_url))
 
             if dirhtml:
@@ -66,6 +73,10 @@ def generate_external_redirects(app, exception):
             logger.info("Writing to '%s'" % redirected_filename)
             with open(redirected_filename, 'w') as f:
                 f.write(TEMPLATE % to_url)
+
+            input_rst_filename = os.path.join(app.srcdir, orig_from_path)
+            with open(input_rst_filename, 'w') as f:
+                f.write(SRC_TEMPLATE % to_url)
 
 
 def setup(app):
