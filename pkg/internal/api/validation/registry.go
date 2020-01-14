ef72b0d8663bbc8674a1286cb32dbf3564b39758
James Munnelly <james@munnelly.eu>
2020-01-14 22:53:19 +0000

    Add validation registry package
    Signed-off-by: James Munnelly <james@munnelly.eu>

---
+++ pkg/internal/api/validation/registry.go
@@ -0,0 +1,220 @@
+/*
+Copyright 2020 The Jetstack cert-manager contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package validation allows a caller to automatically register, lookup and
+// call API validation functions.
+// It is similar to runtime.Scheme and is designed to make writing and
+// consuming API validation functions easier.
+package validation
+
+import (
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+)
+
+// Registry is used to store and lookup references to validation functions for
+// given Kubernetes API types.
+type Registry struct {
+	scheme                 *runtime.Scheme
+	validateRegister       map[schema.GroupVersionKind]ValidateFunc
+	validateUpdateRegister map[schema.GroupVersionKind]ValidateUpdateFunc
+}
+
+type ValidateFunc func(obj runtime.Object) field.ErrorList
+type ValidateUpdateFunc func(oldObj, obj runtime.Object) field.ErrorList
+
+// NewRegistry creates a new empty registry, backed by the provided Scheme.
+func NewRegistry(scheme *runtime.Scheme) *Registry {
+	return &Registry{
+		scheme:                 scheme,
+		validateRegister:       make(map[schema.GroupVersionKind]ValidateFunc),
+		validateUpdateRegister: make(map[schema.GroupVersionKind]ValidateUpdateFunc),
+	}
+}
+
+// AddValidateFunc will add a new validation function to the register.
+// The function will be run whenever Validate is called with a requestVersion
+// set to any recognised GroupVersionKinds for this object.
+// If obj is part of an internal API version, the validation function will be
+// called on all calls to Validate regardless of version.
+// If obj cannot be recognised using the registry's scheme, an error will be
+// returned.
+func (r *Registry) AddValidateFunc(obj runtime.Object, fn ValidateFunc) error {
+	gvks, _, err := r.scheme.ObjectKinds(obj)
+	if err != nil {
+		return err
+	}
+
+	for _, gvk := range gvks {
+		r.appendValidate(gvk, fn)
+	}
+
+	return nil
+}
+
+// AddValidateUpdateFunc will add a new validation function to the register.
+// The function will be run whenever ValidateUpdate is called with a
+// requestVersion set to any recognised GroupVersionKinds for this object.
+// If obj is part of an internal API version, the validation function will be
+// called on all calls to Validate regardless of version.
+// If obj cannot be recognised using the registry's scheme, an error will be
+// returned.
+func (r *Registry) AddValidateUpdateFunc(obj runtime.Object, fn ValidateUpdateFunc) error {
+	gvks, _, err := r.scheme.ObjectKinds(obj)
+	if err != nil {
+		return err
+	}
+
+	for _, gvk := range gvks {
+		r.appendValidateUpdate(gvk, fn)
+	}
+
+	return nil
+}
+
+// Validate will run all validation functions registered for the given object.
+// If the passed obj is *not* of the same version as the provided
+// requestVersion, the registry will attempt to convert the object before
+// calling the validation functions.
+// Any validation functions registered for the objects internal API version
+// will be run against the object regardless of version.
+func (r *Registry) Validate(obj runtime.Object, requestVersion schema.GroupVersionKind) field.ErrorList {
+	versioned, internal := r.lookupValidateFuncs(requestVersion)
+	if versioned == nil && internal == nil {
+		return nil
+	}
+
+	targetObj, internalObj, err := r.convert(obj, requestVersion)
+	if err != nil {
+		return internalError(err)
+	}
+
+	el := field.ErrorList{}
+	if versioned != nil {
+		el = append(el, versioned(targetObj)...)
+	}
+	if internal != nil {
+		el = append(el, internal(internalObj)...)
+	}
+
+	return el
+}
+
+// ValidateUpdate will run all update validation functions registered for the
+// given object.
+// If the passed objects are *not* of the same version as the provided
+// requestVersion, the registry will attempt to convert the objects before
+// calling the validation functions.
+// Any validation functions registered for the objects internal API version
+// will be run against the object regardless of version.
+func (r *Registry) ValidateUpdate(oldObj, obj runtime.Object, requestVersion schema.GroupVersionKind) field.ErrorList {
+	versioned, internal := r.lookupValidateUpdateFuncs(requestVersion)
+	if versioned == nil && internal == nil {
+		return nil
+	}
+
+	targetOldObj, internalOldObj, err := r.convert(oldObj, requestVersion)
+	if err != nil {
+		return internalError(err)
+	}
+
+	targetObj, internalObj, err := r.convert(obj, requestVersion)
+	if err != nil {
+		return internalError(err)
+	}
+
+	el := field.ErrorList{}
+	if versioned != nil {
+		el = append(el, versioned(targetOldObj, targetObj)...)
+	}
+	if internal != nil {
+		el = append(el, internal(internalOldObj, internalObj)...)
+	}
+
+	return el
+}
+
+func (r *Registry) lookupValidateFuncs(gvk schema.GroupVersionKind) (versioned ValidateFunc, internal ValidateFunc) {
+	versioned = r.validateRegister[gvk]
+	gvk.Version = runtime.APIVersionInternal
+	internal = r.validateRegister[gvk]
+	return versioned, internal
+}
+
+func (r *Registry) lookupValidateUpdateFuncs(gvk schema.GroupVersionKind) (versioned ValidateUpdateFunc, internal ValidateUpdateFunc) {
+	versioned = r.validateUpdateRegister[gvk]
+	gvk.Version = runtime.APIVersionInternal
+	internal = r.validateUpdateRegister[gvk]
+	return versioned, internal
+}
+
+func (r *Registry) appendValidate(gvk schema.GroupVersionKind, fn ValidateFunc) {
+	existing, ok := r.validateRegister[gvk]
+	if !ok {
+		r.validateRegister[gvk] = fn
+		return
+	}
+
+	r.validateRegister[gvk] = func(obj runtime.Object) field.ErrorList {
+		return append(existing(obj), fn(obj)...)
+	}
+}
+
+func (r *Registry) appendValidateUpdate(gvk schema.GroupVersionKind, fn ValidateUpdateFunc) {
+	existing, ok := r.validateUpdateRegister[gvk]
+	if !ok {
+		r.validateUpdateRegister[gvk] = fn
+		return
+	}
+
+	r.validateUpdateRegister[gvk] = func(oldObj, obj runtime.Object) field.ErrorList {
+		return append(existing(oldObj, obj), fn(oldObj, obj)...)
+	}
+}
+
+// convert will convert the given obj into the requestVersion as well as
+// returning the internal representation of the object.
+func (r *Registry) convert(obj runtime.Object, requestVersion schema.GroupVersionKind) (targetObj, internalObj runtime.Object, err error) {
+	// create a new object in the desired version
+	targetObj, err = r.scheme.New(requestVersion)
+	if err != nil {
+		return nil, nil, err
+	}
+	// create a new object in the 'internal' version
+	internalVersion := requestVersion
+	internalVersion.Version = runtime.APIVersionInternal
+	internalObj, err = r.scheme.New(internalVersion)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// convert the obj into the internalVersion first
+	if err := r.scheme.Convert(obj, internalObj, nil); err != nil {
+		return nil, nil, err
+	}
+
+	// convert the internalObj into the requestVersion
+	if err := r.scheme.Convert(internalObj, targetObj, nil); err != nil {
+		return nil, nil, err
+	}
+
+	return targetObj, internalObj, nil
+}
+
+func internalError(err error) field.ErrorList {
+	return field.ErrorList{field.InternalError(nil, err)}
+}
---
+++ pkg/internal/api/validation/registry_test.go
@@ -0,0 +1,199 @@
+/*
+Copyright 2020 The Jetstack cert-manager contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package validation_test
+
+import (
+	"fmt"
+	"testing"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+
+	cmapi "github.com/jetstack/cert-manager/pkg/apis/certmanager/v1alpha2"
+	"github.com/jetstack/cert-manager/pkg/internal/api/validation"
+	cmapiinternal "github.com/jetstack/cert-manager/pkg/internal/apis/certmanager"
+	"github.com/jetstack/cert-manager/pkg/webhook"
+)
+
+var (
+	// use the webhook's Scheme during test fixtures as it has all internal and
+	// external cert-manager kinds registered
+	scheme = webhook.Scheme
+)
+
+func TestValidateType(t *testing.T) {
+	reg := validation.NewRegistry(scheme)
+	called := false
+	utilruntime.Must(reg.AddValidateFunc(&cmapi.Certificate{}, func(obj runtime.Object) field.ErrorList {
+		called = true
+		return nil
+	}))
+	errs := reg.Validate(&cmapi.Certificate{}, cmapi.SchemeGroupVersion.WithKind("Certificate"))
+	if len(errs) > 0 {
+		t.Errorf("expected to not get an error but got: %v", errs.ToAggregate())
+	}
+	if !called {
+		t.Errorf("expected registered validation function to run but it did not")
+	}
+}
+
+func TestValidateTypeMultiple(t *testing.T) {
+	reg := validation.NewRegistry(scheme)
+	called1 := false
+	called2 := false
+	calledInternal := false
+	utilruntime.Must(reg.AddValidateFunc(&cmapi.Certificate{}, func(obj runtime.Object) field.ErrorList {
+		called1 = true
+		return nil
+	}))
+	utilruntime.Must(reg.AddValidateFunc(&cmapi.Certificate{}, func(obj runtime.Object) field.ErrorList {
+		called2 = true
+		return nil
+	}))
+	utilruntime.Must(reg.AddValidateFunc(&cmapiinternal.Certificate{}, func(obj runtime.Object) field.ErrorList {
+		calledInternal = true
+		return nil
+	}))
+	errs := reg.Validate(&cmapi.Certificate{}, cmapi.SchemeGroupVersion.WithKind("Certificate"))
+	if len(errs) > 0 {
+		t.Errorf("expected to not get an error but got: %v", errs.ToAggregate())
+	}
+	if !called1 || !called2 {
+		t.Errorf("expected registered validation function to run but it did not")
+	}
+	if !calledInternal {
+		t.Errorf("expected registered internal validation function to run against external type but it did not")
+	}
+}
+
+func TestValidateUpdateTypeMultiple(t *testing.T) {
+	reg := validation.NewRegistry(scheme)
+	called1 := false
+	called2 := false
+	calledInternal := false
+	utilruntime.Must(reg.AddValidateUpdateFunc(&cmapi.Certificate{}, func(_, _ runtime.Object) field.ErrorList {
+		called1 = true
+		return nil
+	}))
+	utilruntime.Must(reg.AddValidateUpdateFunc(&cmapi.Certificate{}, func(_, _ runtime.Object) field.ErrorList {
+		called2 = true
+		return nil
+	}))
+	utilruntime.Must(reg.AddValidateUpdateFunc(&cmapiinternal.Certificate{}, func(_, _ runtime.Object) field.ErrorList {
+		calledInternal = true
+		return nil
+	}))
+	errs := reg.ValidateUpdate(&cmapi.Certificate{}, &cmapi.Certificate{}, cmapi.SchemeGroupVersion.WithKind("Certificate"))
+	if len(errs) > 0 {
+		t.Errorf("expected to not get an error but got: %v", errs.ToAggregate())
+	}
+	if !called1 || !called2 {
+		t.Errorf("expected registered validation function to run but it did not")
+	}
+	if !calledInternal {
+		t.Errorf("expected registered internal validation function to run against external type but it did not")
+	}
+}
+
+func TestValidateUpdateType(t *testing.T) {
+	reg := validation.NewRegistry(scheme)
+	called := false
+	utilruntime.Must(reg.AddValidateUpdateFunc(&cmapi.Certificate{}, func(oldObj, obj runtime.Object) field.ErrorList {
+		called = true
+		return nil
+	}))
+	errs := reg.ValidateUpdate(&cmapi.Certificate{}, &cmapi.Certificate{}, cmapi.SchemeGroupVersion.WithKind("Certificate"))
+	if len(errs) > 0 {
+		t.Errorf("expected to not get an error but got: %v", errs.ToAggregate())
+	}
+	if !called {
+		t.Errorf("expected registered validation function to run but it did not")
+	}
+}
+
+func TestValidateTypeReturnsErrors(t *testing.T) {
+	reg := validation.NewRegistry(scheme)
+	called := false
+	expectedErr := field.InternalError(nil, fmt.Errorf("failed"))
+	utilruntime.Must(reg.AddValidateFunc(&cmapi.Certificate{}, func(obj runtime.Object) field.ErrorList {
+		called = true
+		return field.ErrorList{expectedErr}
+	}))
+	errs := reg.Validate(&cmapi.Certificate{}, cmapi.SchemeGroupVersion.WithKind("Certificate"))
+	if len(errs) != 1 {
+		t.Error("expected to get an error but got none")
+	} else if err := errs[0]; err.Error() != expectedErr.Error() {
+		t.Errorf("expected error to be %q but got %q", expectedErr.Error(), err.Error())
+	}
+	if !called {
+		t.Errorf("expected registered validation function to run but it did not")
+	}
+}
+
+func TestValidateInternalType(t *testing.T) {
+	reg := validation.NewRegistry(scheme)
+	called := false
+	utilruntime.Must(reg.AddValidateFunc(&cmapiinternal.Certificate{}, func(obj runtime.Object) field.ErrorList {
+		called = true
+		return nil
+	}))
+	errs := reg.Validate(&cmapi.Certificate{}, cmapi.SchemeGroupVersion.WithKind("Certificate"))
+	if len(errs) > 0 {
+		t.Errorf("expected to not get an error but got: %v", errs.ToAggregate())
+	}
+	if !called {
+		t.Errorf("expected registered internal validation function to run for external type but it did not")
+	}
+}
+
+func TestValidateNoErrorNoneRegistered(t *testing.T) {
+	reg := validation.NewRegistry(scheme)
+	errs := reg.Validate(&cmapi.Certificate{}, cmapi.SchemeGroupVersion.WithKind("Certificate"))
+	if len(errs) > 0 {
+		t.Errorf("expected to not get an error but got: %v", errs.ToAggregate())
+	}
+}
+
+func TestValidateUpdateNoErrorNoneRegistered(t *testing.T) {
+	reg := validation.NewRegistry(scheme)
+	errs := reg.ValidateUpdate(&cmapi.Certificate{}, &cmapi.Certificate{}, cmapi.SchemeGroupVersion.WithKind("Certificate"))
+	if len(errs) > 0 {
+		t.Errorf("expected to not get an error but got: %v", errs.ToAggregate())
+	}
+}
+
+func TestValidateUnrecognisedType(t *testing.T) {
+	reg := validation.NewRegistry(scheme)
+	err := reg.AddValidateFunc(&corev1.Pod{}, func(obj runtime.Object) field.ErrorList {
+		return nil
+	})
+	if err == nil {
+		t.Errorf("expected to get an error but did not")
+	}
+}
+
+func TestValidateUpdateUnrecognisedType(t *testing.T) {
+	reg := validation.NewRegistry(scheme)
+	err := reg.AddValidateUpdateFunc(&corev1.Pod{}, func(oldObj, obj runtime.Object) field.ErrorList {
+		return nil
+	})
+	if err == nil {
+		t.Errorf("expected to get an error but did not")
+	}
+}
