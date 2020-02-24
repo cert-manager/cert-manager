b28da123e4d92c1e3d229e65d7985f25f42795b0
Josh Soref <jsoref@users.noreply.github.com>
2020-02-24 17:36:08 -0500

    spelling: objects
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- pkg/apis/certmanager/v1alpha3/types.go
+++ pkg/apis/certmanager/v1alpha3/types.go
@@ -102,7 +102,7 @@ const (
 const (
 	// VenafiCustomFieldsAnnotationKey is the annotation that passes on JSON encoded custom fields to the Venafi issuer
 	// This will only work with Venafi TPP v19.3 and higher
-	// The value is an array with objetcs containing the name and value keys
+	// The value is an array with objects containing the name and value keys
 	// for example: `[{"name": "custom-field", "value": "custom-value"}]`
 	VenafiCustomFieldsAnnotationKey = "venafi.cert-manager.io/custom-fields"
 )
