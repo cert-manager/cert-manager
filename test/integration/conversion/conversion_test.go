4e5f9bc31d0cb1cede32c84053be4596749cfc52
James Munnelly <james@munnelly.eu>
2020-02-27 18:14:49 +0000

    Add integration testing framework and a basic conversion test
    Signed-off-by: James Munnelly <james@munnelly.eu>

---
+++ test/integration/conversion/conversion_test.go
@@ -0,0 +1,91 @@
+/*
+Copyright 2020 The Jetstack cert-manager contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package conversion
+
+import (
+	"context"
+	"testing"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/jetstack/cert-manager/pkg/api"
+	"github.com/jetstack/cert-manager/pkg/apis/certmanager/v1alpha2"
+	"github.com/jetstack/cert-manager/pkg/apis/certmanager/v1alpha3"
+	cmmeta "github.com/jetstack/cert-manager/pkg/apis/meta/v1"
+	"github.com/jetstack/cert-manager/test/integration/framework"
+)
+
+func TestConversion(t *testing.T) {
+	tests := map[string]struct {
+		input  runtime.Object
+		output runtime.Object
+	}{
+		"should convert Certificates from v1alpha2 to v1alpha3": {
+			input: &v1alpha2.Certificate{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test",
+					Namespace: "default",
+				},
+				Spec: v1alpha2.CertificateSpec{
+					SecretName: "something",
+					IssuerRef: cmmeta.ObjectReference{
+						Name: "issuername",
+					},
+				},
+			},
+			output: &v1alpha3.Certificate{},
+		},
+		"should convert CertificateRequest from v1alpha2 to v1alpha3": {
+			input: &v1alpha2.CertificateRequest{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test",
+					Namespace: "default",
+				},
+				Spec: v1alpha2.CertificateRequestSpec{
+					// validating webhook isn't currently configured in test
+					// environment so this passes validation.
+					CSRPEM: []byte("a"),
+					IssuerRef: cmmeta.ObjectReference{
+						Name: "issuername",
+					},
+				},
+			},
+			output: &v1alpha3.CertificateRequest{},
+		},
+	}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			config, stop := framework.RunControlPlane(t)
+			defer stop()
+
+			cl, err := client.New(config, client.Options{Scheme: api.Scheme})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if err := cl.Create(context.Background(), test.input); err != nil {
+				t.Fatal(err)
+			}
+			meta := test.input.(metav1.ObjectMetaAccessor)
+			if err := cl.Get(context.Background(), client.ObjectKey{Name: meta.GetObjectMeta().GetName(), Namespace: meta.GetObjectMeta().GetNamespace()}, test.output); err != nil {
+				t.Errorf("failed to fetch object in expected API version: %v", err)
+			}
+		})
+	}
+}
---
+++ test/integration/framework/apiserver.go
@@ -0,0 +1,149 @@
+/*
+Copyright 2020 The Jetstack cert-manager contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package framework
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions"
+	apiextensionsinstall "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/install"
+	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	jsonserializer "k8s.io/apimachinery/pkg/runtime/serializer/json"
+	"k8s.io/apimachinery/pkg/runtime/serializer/versioning"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/rest"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+
+	webhooktesting "github.com/jetstack/cert-manager/cmd/webhook/app/testing"
+)
+
+func init() {
+	// Set environment variables for controller-runtime's envtest package.
+	// This is done once as we cannot scope environment variables to a single
+	// invocation of RunControlPlane due to envtest's design.
+	setUpEnvTestEnv()
+}
+
+type StopFunc func()
+
+func RunControlPlane(t *testing.T) (*rest.Config, StopFunc) {
+	webhookOpts, stopWebhook := webhooktesting.StartWebhookServer(t, []string{})
+	crdsDir, err := getCRDsPath()
+	if err != nil {
+		t.Fatalf("error determining CRD directory path: %v", err)
+	}
+	crds := readCustomResourcesAtPath(t, crdsDir)
+	for _, crd := range crds {
+		t.Logf("Found CRD with name %q", crd.Name)
+	}
+	patchCRDConversion(crds, webhookOpts.URL, webhookOpts.CAPEM)
+	// environment variables
+	env := &envtest.Environment{
+		AttachControlPlaneOutput: true,
+		CRDs:                     crds,
+	}
+	config, err := env.Start()
+	if err != nil {
+		t.Fatalf("failed to start control plane: %v", err)
+	}
+	// TODO: configure Validating and Mutating webhook
+	return config, func() {
+		defer stopWebhook()
+		if err := env.Stop(); err != nil {
+			t.Logf("failed to shut down control plane, not failing test: %v", err)
+		}
+	}
+}
+
+var (
+	internalScheme = runtime.NewScheme()
+)
+
+func init() {
+	utilruntime.Must(metav1.AddMetaToScheme(internalScheme))
+	apiextensionsinstall.Install(internalScheme)
+}
+
+func patchCRDConversion(crds []*v1beta1.CustomResourceDefinition, url string, caPEM []byte) {
+	for _, crd := range crds {
+		if crd.Spec.Conversion == nil {
+			continue
+		}
+		if crd.Spec.Conversion.WebhookClientConfig == nil {
+			continue
+		}
+		if crd.Spec.Conversion.WebhookClientConfig.Service == nil {
+			continue
+		}
+		path := ""
+		if crd.Spec.Conversion.WebhookClientConfig.Service.Path != nil {
+			path = *crd.Spec.Conversion.WebhookClientConfig.Service.Path
+		}
+		url := fmt.Sprintf("%s%s", url, path)
+		crd.Spec.Conversion.WebhookClientConfig.URL = &url
+		crd.Spec.Conversion.WebhookClientConfig.CABundle = caPEM
+		crd.Spec.Conversion.WebhookClientConfig.Service = nil
+	}
+}
+
+func readCustomResourcesAtPath(t *testing.T, path string) []*v1beta1.CustomResourceDefinition {
+	serializer := jsonserializer.NewSerializerWithOptions(jsonserializer.DefaultMetaFactory, internalScheme, internalScheme, jsonserializer.SerializerOptions{
+		Yaml: true,
+	})
+	converter := runtime.UnsafeObjectConvertor(internalScheme)
+	codec := versioning.NewCodec(serializer, serializer, converter, internalScheme, internalScheme, internalScheme, runtime.InternalGroupVersioner, runtime.InternalGroupVersioner, internalScheme.Name())
+
+	var crds []*v1beta1.CustomResourceDefinition
+	if err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if filepath.Ext(path) != ".yaml" {
+			return nil
+		}
+		crd, err := readCRDAtPath(codec, converter, path)
+		if err != nil {
+			return err
+		}
+		crds = append(crds, crd)
+		return nil
+	}); err != nil {
+		t.Fatal(err)
+	}
+	return crds
+}
+
+func readCRDAtPath(codec runtime.Codec, converter runtime.ObjectConvertor, path string) (*v1beta1.CustomResourceDefinition, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	internalCRD := &apiextensions.CustomResourceDefinition{}
+	if _, _, err := codec.Decode(data, nil, internalCRD); err != nil {
+		return nil, err
+	}
+
+	output := &v1beta1.CustomResourceDefinition{}
+	return output, converter.Convert(internalCRD, output, nil)
+}
