28639c366bfa9adf4ec9e94bf0191581c49b89b0
JoshVanL <vleeuwenjoshua@gmail.com>
2020-04-27 10:19:49 +0100

    Changes ctl renew integration test to only test for issuing condition
    Signed-off-by: JoshVanL <vleeuwenjoshua@gmail.com>

--- test/integration/ctl/ctl_renew_test.go
+++ test/integration/ctl/ctl_renew_test.go
@@ -18,28 +18,18 @@ package certificates
 
 import (
 	"context"
-	"encoding/pem"
-	"fmt"
-	"os"
 	"testing"
 	"time"
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
-	"k8s.io/client-go/tools/cache"
-	"k8s.io/client-go/util/workqueue"
-	"k8s.io/utils/clock"
 
 	"github.com/jetstack/cert-manager/cmd/ctl/pkg/renew"
 	apiutil "github.com/jetstack/cert-manager/pkg/api/util"
 	cmapi "github.com/jetstack/cert-manager/pkg/apis/certmanager/v1alpha2"
 	cmmeta "github.com/jetstack/cert-manager/pkg/apis/meta/v1"
-	controllerpkg "github.com/jetstack/cert-manager/pkg/controller"
-	"github.com/jetstack/cert-manager/pkg/controller/expcertificates/issuing"
-	"github.com/jetstack/cert-manager/pkg/controller/expcertificates/readiness"
-	"github.com/jetstack/cert-manager/pkg/controller/expcertificates/trigger/policies"
-	logf "github.com/jetstack/cert-manager/pkg/logs"
 	utilpki "github.com/jetstack/cert-manager/pkg/util/pki"
 	"github.com/jetstack/cert-manager/test/integration/framework"
 	"github.com/jetstack/cert-manager/test/unit/gen"
@@ -55,15 +45,10 @@ func TestCtlRenew(t *testing.T) {
 	defer cancel()
 
 	// Build, instantiate and run the issuing controller.
-	kubeClient, factory, cmCl, cmFactory := framework.NewClients(t, config)
-	controllerOptions := controllerpkg.CertificateOptions{
-		EnableOwnerRef: true,
-	}
-	recorder := framework.NewEventRecorder(t)
+	kubeClient, _, cmCl, _ := framework.NewClients(t, config)
 
 	var (
 		crtName                  = "testcrt"
-		revision                 = 1
 		namespace                = "testns"
 		nextPrivateKeySecretName = "next-private-key-test-crt"
 		secretName               = "test-crt-tls"
@@ -106,108 +91,37 @@ func TestCtlRenew(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	// Init and start controllers
-	ctrlIssuing, queueIssuing, mustSyncIssuing := issuing.NewController(logf.Log, kubeClient, cmCl, factory, cmFactory, recorder, clock.RealClock{}, controllerOptions)
-	ctrlReadiness, queueReadiness, mustSyncReadiness := readiness.NewController(logf.Log, cmCl, factory, cmFactory, policies.TriggerPolicyChain)
-
-	for _, ctrl := range []struct {
-		syncFunc func(ctx context.Context, key string) error
-		mustSync []cache.InformerSynced
-		queue    workqueue.RateLimitingInterface
-	}{
-		{ctrlIssuing.ProcessItem, mustSyncIssuing, queueIssuing},
-		{ctrlReadiness.ProcessItem, mustSyncReadiness, queueReadiness},
-	} {
-		c := controllerpkg.NewController(
-			context.Background(),
-			ctrl.syncFunc,
-			ctrl.mustSync,
-			nil,
-			ctrl.queue,
-		)
-
-		stopController := framework.StartInformersAndController(t, factory, cmFactory, c)
-		defer stopController()
-	}
-
-	// Add Issuing condition to Certificate
-	apiutil.SetCertificateCondition(crt, cmapi.CertificateConditionIssuing, cmmeta.ConditionTrue, "", "")
-	crt.Status.NextPrivateKeySecretName = &nextPrivateKeySecretName
-	crt.Status.Revision = &revision
-	crt, err = cmCl.CertmanagerV1alpha2().Certificates(namespace).UpdateStatus(ctx, crt, metav1.UpdateOptions{})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Create x509 CSR from Certificate
-	csr, err := utilpki.GenerateCSR(crt)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Encode CSR
-	csrDER, err := utilpki.EncodeCSR(csr, sk)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	csrPEM := pem.EncodeToMemory(&pem.Block{
-		Type: "CERTIFICATE REQUEST", Bytes: csrDER,
-	})
-
-	// Sign Certificate
-	certTemplate, err := utilpki.GenerateTemplate(crt)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Sign and encode the certificate
-	certPem, _, err := utilpki.SignCertificate(certTemplate, certTemplate, sk.Public(), sk)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Create CertificateRequest
-	req := gen.CertificateRequest(crtName,
-		gen.SetCertificateRequestNamespace(namespace),
-		gen.SetCertificateRequestCSR(csrPEM),
-		gen.SetCertificateRequestIssuer(crt.Spec.IssuerRef),
-		gen.SetCertificateRequestAnnotations(map[string]string{
-			cmapi.CertificateRequestRevisionAnnotationKey: fmt.Sprintf("%d", revision+1),
-		}),
-		gen.AddCertificateRequestOwnerReferences(*metav1.NewControllerRef(
-			crt,
-			cmapi.SchemeGroupVersion.WithKind("Certificate"),
-		)),
-	)
-	req, err = cmCl.CertmanagerV1alpha2().CertificateRequests(namespace).Create(ctx, req, metav1.CreateOptions{})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Set CertificateRequest as ready
-	req.Status.CA = certPem
-	req.Status.Certificate = certPem
-	apiutil.SetCertificateRequestCondition(req, cmapi.CertificateRequestConditionReady, cmmeta.ConditionTrue, cmapi.CertificateRequestReasonIssued, "")
-	req, err = cmCl.CertmanagerV1alpha2().CertificateRequests(namespace).UpdateStatus(ctx, req, metav1.UpdateOptions{})
-	if err != nil {
-		t.Fatal(err)
-	}
-
 	// Run ctl renew command and wait for ready
 	streams, _, _, _ := genericclioptions.NewTestIOStreams()
-	streams.Out = os.Stdout
 
 	cmd := &renew.Options{
 		Namespace:  "testns",
 		CMClient:   cmCl,
 		RestConfig: config,
-		Wait:       true,
-		Timeout:    time.Second * 10,
 		IOStreams:  streams,
 	}
 
 	if err := cmd.Run([]string{"testcrt"}); err != nil {
 		t.Fatal(err)
+	}
+
+	// Wait for the Certificate to have the 'Issuing' condition set
+	err = wait.Poll(time.Millisecond*100, time.Second*5, func() (done bool, err error) {
+		crt, err = cmCl.CertmanagerV1alpha2().Certificates(namespace).Get(ctx, crtName, metav1.GetOptions{})
+		if err != nil {
+			t.Logf("Failed to fetch Certificate resource, retrying: %v", err)
+			return false, nil
+		}
+
+		if cond := apiutil.GetCertificateCondition(crt, cmapi.CertificateConditionIssuing); cond == nil || cond.Status != cmmeta.ConditionTrue {
+			t.Logf("Certificate does not have expected condition, got=%#v", cond)
+			return false, nil
+		}
+
+		return true, nil
+	})
+
+	if err != nil {
+		t.Fatalf("Failed to wait for final state: %+v", crt)
 	}
 }
