4dd70a6fe70370b34e6a1e0e6a6375b0a060ae7a
JoshVanL <vleeuwenjoshua@gmail.com>
2020-05-18 17:43:18 +0100

    Adds new metrics controller to reconcile over Certificates and its Status (Ready and Expiry)
    Signed-off-by: JoshVanL <vleeuwenjoshua@gmail.com>

---
+++ pkg/controller/metrics/controller.go
@@ -0,0 +1,124 @@
+/*
+Copyright 2019 The Jetstack cert-manager contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package metrics
+
+import (
+	"context"
+	"time"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/workqueue"
+
+	cminformers "github.com/jetstack/cert-manager/pkg/client/informers/externalversions"
+	cmlisters "github.com/jetstack/cert-manager/pkg/client/listers/certmanager/v1alpha2"
+	controllerpkg "github.com/jetstack/cert-manager/pkg/controller"
+	"github.com/jetstack/cert-manager/pkg/metrics"
+)
+
+const (
+	ControllerName = "metrics"
+)
+
+// controllerWrapper wraps the `controller` structure to make it implement
+// the controllerpkg.queueingController interface
+type controllerWrapper struct {
+	*controller
+}
+
+// This controller is synced on all Certificate 'create', 'update', and
+// 'delete' events which will update the metrics for that Certificate.
+type controller struct {
+	certificateLister cmlisters.CertificateLister
+
+	metrics *metrics.Metrics
+}
+
+func NewController(
+	factory informers.SharedInformerFactory,
+	cmFactory cminformers.SharedInformerFactory,
+	metrics *metrics.Metrics,
+) (*controller, workqueue.RateLimitingInterface, []cache.InformerSynced) {
+	// create a queue used to queue up items to be processed
+	queue := workqueue.NewNamedRateLimitingQueue(workqueue.NewItemExponentialFailureRateLimiter(time.Second*1, time.Second*30), ControllerName)
+
+	// obtain references to all the informers used by this controller
+	certificateInformer := cmFactory.Certmanager().V1alpha2().Certificates()
+
+	// Reconcile over all Certificate events. We do _not_ reconcile on Secret
+	// events that are related to Certificates. It is the responsibility of the
+	// Certificates controllers to update accordingly.
+	certificateInformer.Informer().AddEventHandler(&controllerpkg.QueuingEventHandler{Queue: queue})
+
+	// build a list of InformerSynced functions that will be returned by the
+	// Register method.  the controller will only begin processing items once all
+	// of these informers have synced.
+	mustSync := []cache.InformerSynced{
+		certificateInformer.Informer().HasSynced,
+	}
+
+	return &controller{
+		certificateLister: certificateInformer.Lister(),
+		metrics:           metrics,
+	}, queue, mustSync
+}
+
+func (c *controller) ProcessItem(ctx context.Context, key string) error {
+	// Set context deadline for full sync in 10 seconds
+	ctx, cancel := context.WithTimeout(ctx, time.Second*10)
+	defer cancel()
+
+	namespace, name, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		return nil
+	}
+
+	crt, err := c.certificateLister.Certificates(namespace).Get(name)
+	if apierrors.IsNotFound(err) {
+		// If the Certificate no longer exists, remove it's metrics from being exposed.
+		c.metrics.RemoveCertificate(key)
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+
+	// Update that Certificates metrics
+	c.metrics.UpdateCertificate(ctx, crt)
+
+	return nil
+}
+
+func (c *controllerWrapper) Register(ctx *controllerpkg.Context) (workqueue.RateLimitingInterface, []cache.InformerSynced, error) {
+	ctrl, queue, mustSync := NewController(
+		ctx.KubeSharedInformerFactory,
+		ctx.SharedInformerFactory,
+		ctx.Metrics,
+	)
+	c.controller = ctrl
+
+	return queue, mustSync, nil
+}
+
+func init() {
+	controllerpkg.Register(ControllerName, func(ctx *controllerpkg.Context) (controllerpkg.Interface, error) {
+		return controllerpkg.NewBuilder(ctx, ControllerName).
+			For(&controllerWrapper{}).
+			Complete()
+	})
+}
