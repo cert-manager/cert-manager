dceae33364f29401670571db9549bfefe5b0c38a
Haoxiang Zhou <haoxiang.zhou@jetstack.io>
2020-05-26 11:23:02 +0100

    Do not add ca.crt key to TLS secret if empty
    Signed-off-by: Haoxiang Zhou <haoxiang.zhou@jetstack.io>

--- pkg/controller/certificates/sync.go
+++ pkg/controller/certificates/sync.go
@@ -877,7 +877,11 @@ func (c *certificateRequestManager) setSecretValues(ctx context.Context, crt *cm
 
 	s.Data[corev1.TLSPrivateKeyKey] = data.pk
 	s.Data[corev1.TLSCertKey] = data.cert
-	s.Data[cmmeta.TLSCAKey] = data.ca
+	if data.ca != nil && len(data.ca) > 0 {
+		s.Data[cmmeta.TLSCAKey] = data.ca
+	} else {
+		delete(s.Data, cmmeta.TLSCAKey)
+	}
 
 	if s.Annotations == nil {
 		s.Annotations = make(map[string]string)
