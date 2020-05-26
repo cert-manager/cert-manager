cc0c0da76fcac16b0b5753a25c702dcfcbc18602
Maartje Eyskens <maartje@eyskens.me>
2020-05-26 16:03:42 +0200

    Check for empty Organizations instead
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

--- pkg/internal/venafi/sign.go
+++ pkg/internal/venafi/sign.go
@@ -153,7 +153,7 @@ func (v *Venafi) Sign(csrPEM []byte, duration time.Duration, customFields []inte
 func newVRequest(cert *x509.Certificate) *certificate.Request {
 	req := certificate.NewRequest(cert)
 
-	if cert.Subject.String() == "" {
+	if len(cert.Subject.Organization) == 0 {
 		// Venafi TPP errors on an empty DN
 		// this applies the pre-0.15 default again if no other DN field is set
 		cert.Subject.Organization = []string{"cert-manager"}
