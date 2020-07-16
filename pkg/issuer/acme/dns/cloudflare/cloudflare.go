06d0d5c4b048ac2f1b07b0a15ede01b921821f9e
Maartje Eyskens <maartje@eyskens.me>
2020-07-16 10:26:05 +0200

    Remove double spacing
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

--- pkg/issuer/acme/dns/cloudflare/cloudflare.go
+++ pkg/issuer/acme/dns/cloudflare/cloudflare.go
@@ -236,7 +236,7 @@ func (c *DNSProvider) makeRequest(method, uri string, body io.Reader) (json.RawM
 	}
 	resp, err := client.Do(req)
 	if err != nil {
-		return nil, fmt.Errorf("Error querying Cloudflare API for  %s %q -> %v", method, uri, err)
+		return nil, fmt.Errorf("Error querying Cloudflare API for %s %q -> %v", method, uri, err)
 	}
 
 	defer resp.Body.Close()
@@ -258,7 +258,7 @@ func (c *DNSProvider) makeRequest(method, uri string, body io.Reader) (json.RawM
 			}
 			return nil, fmt.Errorf("Cloudflare API Error for %s %q \n%s", method, uri, errStr)
 		}
-		return nil, fmt.Errorf("Cloudflare API error for  %s %q", method, uri)
+		return nil, fmt.Errorf("Cloudflare API error for %s %q", method, uri)
 	}
 
 	return r.Result, nil
