3c7573ac12c598d302daed347b699a13bba6e1a2
Maartje Eyskens <maartje@eyskens.me>
2020-07-29 19:20:00 +0200

    Use crt variable to update certificate
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

--- pkg/controller/ingress-shim/sync.go
+++ pkg/controller/ingress-shim/sync.go
@@ -185,13 +185,7 @@ func (c *controller) buildCertificates(ctx context.Context, ing *extv1beta1.Ingr
 
 			updateCrt := existingCrt.DeepCopy()
 
-			updateCrt.Spec.DNSNames = tls.Hosts
-			updateCrt.Spec.SecretName = tls.SecretName
-			updateCrt.Spec.IssuerRef.Name = issuerName
-			updateCrt.Spec.IssuerRef.Kind = issuerKind
-			updateCrt.Spec.IssuerRef.Group = issuerGroup
-			updateCrt.Spec.CommonName = ""
-			updateCrt.Labels = ing.Labels
+			updateCrt.Spec = crt.Spec
 			err = c.setIssuerSpecificConfig(updateCrt, ing, tls)
 			if err != nil {
 				return nil, nil, err
