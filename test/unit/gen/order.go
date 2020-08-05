57770b1f583e36e6d0a834f19e80f2fb8b9925cf
Haoxiang Zhou <haoxiang.zhou@jetstack.io>
2020-08-05 13:58:19 +0100

    User order as variable name in gen/order.go
    Signed-off-by: Haoxiang Zhou <haoxiang.zhou@jetstack.io>

--- test/unit/gen/order.go
+++ test/unit/gen/order.go
@@ -24,74 +24,74 @@ import (
 type OrderModifier func(*cmacme.Order)
 
 func Order(name string, mods ...OrderModifier) *cmacme.Order {
-	c := &cmacme.Order{
+	order := &cmacme.Order{
 		ObjectMeta: ObjectMeta(name),
 	}
 	for _, mod := range mods {
-		mod(c)
+		mod(order)
 	}
-	return c
+	return order
 }
 
-func OrderFrom(crt *cmacme.Order, mods ...OrderModifier) *cmacme.Order {
-	crt = crt.DeepCopy()
+func OrderFrom(order *cmacme.Order, mods ...OrderModifier) *cmacme.Order {
+	order = order.DeepCopy()
 	for _, mod := range mods {
-		mod(crt)
+		mod(order)
 	}
-	return crt
+	return order
 }
 
 // SetIssuer sets the Order.spec.issuerRef field
 func SetOrderIssuer(o cmmeta.ObjectReference) OrderModifier {
-	return func(c *cmacme.Order) {
-		c.Spec.IssuerRef = o
+	return func(order *cmacme.Order) {
+		order.Spec.IssuerRef = o
 	}
 }
 
 func SetOrderDNSNames(dnsNames ...string) OrderModifier {
-	return func(crt *cmacme.Order) {
-		crt.Spec.DNSNames = dnsNames
+	return func(order *cmacme.Order) {
+		order.Spec.DNSNames = dnsNames
 	}
 }
 
 func SetOrderURL(url string) OrderModifier {
-	return func(crt *cmacme.Order) {
-		crt.Status.URL = url
+	return func(order *cmacme.Order) {
+		order.Status.URL = url
 	}
 }
 
 func SetOrderState(s cmacme.State) OrderModifier {
-	return func(crt *cmacme.Order) {
-		crt.Status.State = s
+	return func(order *cmacme.Order) {
+		order.Status.State = s
 	}
 }
 
 func SetOrderReason(reason string) OrderModifier {
-	return func(crt *cmacme.Order) {
-		crt.Status.Reason = reason
+	return func(order *cmacme.Order) {
+		order.Status.Reason = reason
 	}
 }
 
 func SetOrderStatus(s cmacme.OrderStatus) OrderModifier {
-	return func(o *cmacme.Order) {
-		o.Status = s
+	return func(order *cmacme.Order) {
+		order.Status = s
 	}
 }
 
 func SetOrderCertificate(d []byte) OrderModifier {
-	return func(crt *cmacme.Order) {
-		crt.Status.Certificate = d
+	return func(order *cmacme.Order) {
+		order.Status.Certificate = d
 	}
 }
 
 func SetOrderCommonName(commonName string) OrderModifier {
-	return func(crt *cmacme.Order) {
-		crt.Spec.CommonName = commonName
+	return func(order *cmacme.Order) {
+		order.Spec.CommonName = commonName
 	}
 }
 
 func SetOrderNamespace(namespace string) OrderModifier {
-	return func(crt *cmacme.Order) {
-		crt.ObjectMeta.Namespace = namespace
+	return func(order *cmacme.Order) {
+		order.ObjectMeta.Namespace = namespace
 	}
 }
