46ab9178edf408c888ebed0175ae20104dc45a78
Haoxiang Zhou <haoxiang.zhou@jetstack.io>
2020-08-28 15:44:53 +0200

    Change order of adding versions to scheme to change priority
    Signed-off-by: Haoxiang Zhou <haoxiang.zhou@jetstack.io>

--- pkg/internal/apis/certmanager/install/install.go
+++ pkg/internal/apis/certmanager/install/install.go
@@ -35,10 +35,10 @@ import (
 // Install registers the API group and adds types to a scheme
 func Install(scheme *runtime.Scheme) {
 	utilruntime.Must(certmanager.AddToScheme(scheme))
-	utilruntime.Must(v1alpha2.AddToScheme(scheme))
-	utilruntime.Must(v1alpha3.AddToScheme(scheme))
-	utilruntime.Must(v1beta1.AddToScheme(scheme))
 	utilruntime.Must(v1.AddToScheme(scheme))
+	utilruntime.Must(v1beta1.AddToScheme(scheme))
+	utilruntime.Must(v1alpha3.AddToScheme(scheme))
+	utilruntime.Must(v1alpha2.AddToScheme(scheme))
 
 	utilruntime.Must(cmmetav1.AddToScheme(scheme))
 }
