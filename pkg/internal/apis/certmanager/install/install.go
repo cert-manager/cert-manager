89a0f252206eba99e2405e0666553baec34d0a87
Maartje Eyskens <maartje@eyskens.me>
2020-08-31 10:11:55 +0200

    run update-all
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

--- pkg/internal/apis/certmanager/install/install.go
+++ pkg/internal/apis/certmanager/install/install.go
@@ -35,7 +35,7 @@ import (
 // Install registers the API group and adds types to a scheme
 func Install(scheme *runtime.Scheme) {
 	utilruntime.Must(certmanager.AddToScheme(scheme))
-       // The first version in this list will be the default version used
+	// The first version in this list will be the default version used
 	utilruntime.Must(v1.AddToScheme(scheme))
 	utilruntime.Must(v1beta1.AddToScheme(scheme))
 	utilruntime.Must(v1alpha3.AddToScheme(scheme))
