ab0cd57dc58fd73a76fd96bd9d1402bd5ae96582
Maartje Eyskens <maartje@eyskens.me>
2020-12-11 19:04:13 +0100

    Use The cert-manager Authors.
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

--- hack/verify-chart-version.sh
+++ hack/verify-chart-version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright 2020 The cert-manager contributors.
+# Copyright 2020 The cert-manager Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
