245d0f5c27ae830708d83a5a6db54944ad42f936
irbekrm <irbekrm@gmail.com>
2021-02-23 17:26:58 +0000

    Pass DefaultRenewBefore into trigger controller
    Signed-off-by: irbekrm <irbekrm@gmail.com>

--- pkg/controller/certificates/trigger/trigger_controller.go
+++ pkg/controller/certificates/trigger/trigger_controller.go
@@ -244,7 +244,7 @@ func (c *controllerWrapper) Register(ctx *controllerpkg.Context) (workqueue.Rate
 		ctx.SharedInformerFactory,
 		ctx.Recorder,
 		ctx.Clock,
-		policies.NewTriggerPolicyChain(ctx.Clock, ctx.CertificateOptions.RenewBeforeExpiryDuration),
+		policies.NewTriggerPolicyChain(ctx.Clock, cmapi.DefaultRenewBefore),
 	)
 	c.controller = ctrl
 
