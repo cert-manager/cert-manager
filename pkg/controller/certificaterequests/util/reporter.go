ff3e4bb07d674820ad1e76d9c4d9ff0e165f5347
joshvanl <vleeuwenjoshua@gmail.com>
2021-04-09 18:19:44 +0100

    Don't fire an event when the Denied ready condition is set
    Signed-off-by: joshvanl <vleeuwenjoshua@gmail.com>

--- pkg/controller/certificaterequests/util/reporter.go
+++ pkg/controller/certificaterequests/util/reporter.go
@@ -67,14 +67,6 @@ func (r *Reporter) Denied(cr *cmapi.CertificateRequest) {
 	}
 
 	message := "The CertificateRequest was denied by an approval controller"
-
-	// If RequestDenied condition not already set then fire a RequestDenied
-	// Event. This is to reduce strain on the API server and avoid rate limiting
-	// ourselves for Event creation.
-	if apiutil.CertificateRequestReadyReason(cr) != cmapi.CertificateRequestReasonDenied {
-		r.recorder.Event(cr, corev1.EventTypeWarning, cmapi.CertificateRequestReasonDenied, message)
-	}
-
 	apiutil.SetCertificateRequestCondition(cr, cmapi.CertificateRequestConditionReady,
 		cmmeta.ConditionFalse, cmapi.CertificateRequestReasonDenied, message)
 }
