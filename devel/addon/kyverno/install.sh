26d4e7d887514df63a0082403bbf823e72d4fb5f
Richard Wall <richard.wall@jetstack.io>
2021-05-20 09:40:15 +0100

    Install Kyverno and all Pod security policies
    Signed-off-by: Richard Wall <richard.wall@jetstack.io>

---
+++ devel/addon/kyverno/install.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Copyright 2020 The cert-manager Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Installs an instance of Kyverno with all Pod security policies enabled.
+# * https://kyverno.io/policies/pod-security/
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE}")
+source "${SCRIPT_ROOT}/../../lib/lib.sh"
+
+check_tool kubectl
+check_tool helm
+check_tool kustomize
+
+# Install latest version of Kyverno
+helm repo add kyverno https://kyverno.github.io/kyverno/
+helm repo update
+helm upgrade --install --wait kyverno kyverno/kyverno --namespace kyverno --create-namespace
+# Install all Pod security policies
+kustomize build https://github.com/kyverno/policies/pod-security | kubectl apply -f -
