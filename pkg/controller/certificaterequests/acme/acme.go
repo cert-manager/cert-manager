1678d0833e9bfeea94271b96d192d2050ae06995
joshvanl <vleeuwenjoshua@gmail.com>
2021-06-02 12:21:50 +0100

    Reverts ACME issuer from forming a chain bundle and populating the ca.crt
    Signed-off-by: joshvanl <vleeuwenjoshua@gmail.com>

--- pkg/controller/certificaterequests/acme/acme.go
+++ pkg/controller/certificaterequests/acme/acme.go
@@ -206,20 +206,12 @@ func (a *ACME) Sign(ctx context.Context, cr *v1.CertificateRequest, issuer v1.Ge
 		return nil, a.acmeClientV.Orders(order.Namespace).Delete(ctx, order.Name, metav1.DeleteOptions{})
 	}
 
-	bundle, err := pki.ParseSingleCertificateChainPEM(order.Status.Certificate)
-	if err != nil {
-		log.Error(err, "failed to successfully build a certificate chain from data on Order resource.")
-		return nil, a.acmeClientV.Orders(order.Namespace).Delete(ctx, order.Name, metav1.DeleteOptions{})
-	}
-
 	log.V(logf.InfoLevel).Info("certificate issued")
 
 	// Order valid, return cert. The calling controller will update with ready if its happy with the cert.
 	return &issuerpkg.IssueResponse{
-		Certificate: bundle.ChainPEM,
-		CA:          bundle.CAPEM,
+		Certificate: order.Status.Certificate,
 	}, nil
-
 }
 
 // Build order. If we error here it is a terminating failure.
