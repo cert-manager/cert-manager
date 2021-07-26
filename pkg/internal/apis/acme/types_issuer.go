f180f0a0e6fd2832c078922cc38e5c8b108974a9
joshvanl <vleeuwenjoshua@gmail.com>
2021-07-26 17:00:08 +0100

    Change internal ACME API to use apiextensions v1beta1 -> v1
    Signed-off-by: joshvanl <vleeuwenjoshua@gmail.com>

--- pkg/internal/apis/acme/types_issuer.go
+++ pkg/internal/apis/acme/types_issuer.go
@@ -18,7 +18,7 @@ package acme
 
 import (
 	corev1 "k8s.io/api/core/v1"
-	apiext "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 
 	cmmeta "github.com/jetstack/cert-manager/pkg/internal/apis/meta"
 )
@@ -474,7 +474,7 @@ type ACMEIssuerDNS01ProviderWebhook struct {
 	// should use a SecretKeySelector to reference a Secret resource.
 	// For details on the schema of this field, consult the webhook provider
 	// implementation's documentation.
-	Config *apiext.JSON
+	Config *apiextensionsv1.JSON
 }
 
 type ACMEIssuerStatus struct {
