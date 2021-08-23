49a6d0a86f9059ae35de2d6a91d6da4bb93e7567
irbekrm <irbekrm@gmail.com>
2021-08-23 15:00:57 +0100

    Adds a test case for renewal time skew and a comment
    Signed-off-by: irbekrm <irbekrm@gmail.com>

--- pkg/controller/certificates/util_test.go
+++ pkg/controller/certificates/util_test.go
@@ -331,6 +331,15 @@ func TestRenewalTime(t *testing.T) {
 			renewBeforeOverride: &metav1.Duration{Duration: time.Hour * 24},
 			expectedRenewalTime: &metav1.Time{Time: now.Add(time.Minute * 3)}, // renew in 3 minutes
 		},
+		// This test case is here to guard against an earlier bug where
+		// a non-truncated renewal time returned from this function
+		// caused certs to not be renewed.
+		// See https://github.com/jetstack/cert-manager/pull/4399
+		"certificate's duration is skewed by a second": {
+			notBefore:           now,
+			notAfter:            now.Add(time.Hour * 24).Add(time.Second * -1),
+			expectedRenewalTime: &metav1.Time{Time: now.Add(time.Hour * 16).Add(time.Second * -1)},
+		},
 	}
 	for n, s := range tests {
 		t.Run(n, func(t *testing.T) {
