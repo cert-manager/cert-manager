cert-manager/cert-manager

commit 6484010f5c8bbd1605e896d316f950be0dc394d9

fix(issuing): wait until req matches cert before setting failure
The issuing controller wasn't checking if the certificate request that
it picked up is up to date. That resulted in the certificate being set
to "Failing" and "Issuing = False" due to an old certificate request
that was created during a previous issuance. The certificate would then
become stale.

Signed-off-by: Maël Valais <mael.valais@gmail.com>

pkg/controller/certificates/issuing/issuing_controller.go
@@ -225,6 +225,17 @@ func (c *controller) ProcessItem(ctx context.Context, key string) error {
 	req := reqs[0]
 	log = logf.WithResource(log, req)
 
+	// Verify the CSR options match what is requested in certificate.spec.
+	// If there are violations in the spec, then the requestmanager will handle this.
+	requestViolations, err := certificates.RequestMatchesSpec(req, crt.Spec)
+	if err != nil {
+		return err
+	}
+	if len(requestViolations) > 0 {
+		log.V(logf.DebugLevel).Info("CertificateRequest does not match Certificate, waiting for keymanager controller")
+		return nil
+	}
+
 	cond := apiutil.GetCertificateRequestCondition(req, cmapi.CertificateRequestConditionReady)
 	if cond == nil {
 		log.V(logf.DebugLevel).Info("CertificateRequest does not have Ready condition, waiting...")
@@ -235,17 +246,6 @@ func (c *controller) ProcessItem(ctx context.Context, key string) error {
 	// and set the Issuing status condition to False with reason.
 	if cond.Reason == cmapi.CertificateRequestReasonFailed {
 		return c.failIssueCertificate(ctx, log, crt, req)
-	}
-
-	// Verify the CSR options match what is requested in certificate.spec.
-	// If there are violations in the spec, then the requestmanager will handle this.
-	requestViolations, err := certificates.RequestMatchesSpec(req, crt.Spec)
-	if err != nil {
-		return err
-	}
-	if len(requestViolations) > 0 {
-		log.V(logf.DebugLevel).Info("CertificateRequest does not match Certificate, waiting for keymanager controller")
-		return nil
 	}
 
 	// If public key does not match, do nothing (requestmanager will handle this).

commit 19cb640f658bbbff60d1d4fd2d8c1636cd357e8b

issuing: reproduce bug where issuing controller picks up oudated req
In 6484010f I made sure that the issuing controller would not use the
status of an outdated CertificateRequest for setting its own status. By
"outdated", I mean that the CertificateRequest was created at a
moment when the Certificate has a different spec.

The bug fix I introduced in 6484010f is only able to detect changes in
the Certificate resource that can be compared to the CertificateRequest.
That includes the following fields in the Certificate:

- spec.commonName
- spec.dnsNames
- spec.ipAddresses
- spec.uris
- spec.emailAddresses
- spec.subject
- spec.isCA
- spec.keyUsages
- spec.duration
- spec.issuerRef

But some fields on the Certificate can't be compared to anything on the
CertificateRequest, making it impossible for the issuing controller to
know when it should not bubble up the CR status into the Certificate.
The information that can't be detected are:

- spec.privateKey.rotationPolicy
- spec.privateKey.encoding
- spec.privateKey.algorithm
- spec.privateKey.size
- spec.encodeUsagesInRequest

Note: some Certificate fields do not need to be compared since they do
not need a re-issuance of the certificate when they are changed:

- spec.keystores.jks
- spec.keystores.pkcs12
- spec.renewBefore
- spec.revisionHistoryLimit

Signed-off-by: Maël Valais <mael@vls.dev>

pkg/controller/certificates/issuing/issuing_controller_test.go
@@ -213,6 +213,44 @@ func TestIssuingController(t *testing.T) {
 			},
 			expectedErr: false,
 		},
+		"if certificate is in Issuing state, one CertificateRequest, but has failed and rotationPolicy changed, do nothing": {
+			certificate: exampleBundle.Certificate,
+			builder: &testpkg.Builder{
+				CertManagerObjects: []runtime.Object{
+					gen.CertificateFrom(issuingCert,
+						gen.SetCertificateRotationPolicy("Never"),
+					),
+					gen.CertificateRequestFrom(
+						internaltest.MustCreateCryptoBundle(t,
+							gen.CertificateFrom(issuingCert,
+								gen.SetCertificateRotationPolicy("Always"), // Mismatch since the cert has "Never".
+							), fixedClock,
+						).CertificateRequest,
+						gen.AddCertificateRequestAnnotations(map[string]string{
+							cmapi.CertificateRequestRevisionAnnotationKey: "2", // Current Certificate revision=1
+						}), gen.SetCertificateRequestStatusCondition(cmapi.CertificateRequestCondition{
+							Type:    cmapi.CertificateRequestConditionReady,
+							Status:  cmmeta.ConditionFalse,
+							Reason:  cmapi.CertificateRequestReasonFailed,
+							Message: "The certificate request failed because of reasons",
+						}),
+					),
+				},
+				KubeObjects: []runtime.Object{
+					&corev1.Secret{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      nextPrivateKeySecretName,
+							Namespace: exampleBundle.Certificate.Namespace,
+						},
+						Data: map[string][]byte{
+							corev1.TLSPrivateKeyKey: exampleBundle.PrivateKeyBytes,
+						},
+					},
+				},
+				ExpectedActions: []testpkg.Action{},
+			},
+			expectedErr: false,
+		},
 
 		"if certificate is in Issuing state, one CertificateRequest, but has failed, set failed state and log event": {
 			certificate: exampleBundle.Certificate,

test/unit/gen/certificate.go
@@ -104,6 +104,12 @@ func SetCertificateKeyEncoding(keyEncoding v1.PrivateKeyEncoding) CertificateMod
 	}
 }
 
+func SetCertificateRotationPolicy(policy v1.PrivateKeyRotationPolicy) CertificateModifier {
+	return func(crt *v1.Certificate) {
+		crt.Spec.PrivateKey.RotationPolicy = policy
+	}
+}
+
 func SetCertificateSecretName(secretName string) CertificateModifier {
 	return func(crt *v1.Certificate) {
 		crt.Spec.SecretName = secretName