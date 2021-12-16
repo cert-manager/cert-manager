57ba9bd71cbb9422bd96452b4ad748f908bfec39
Richard Wall <richard.wall@jetstack.io>
2021-12-16 15:54:15 +0000

    Register the deprecated API kinds
    Signed-off-by: Richard Wall <richard.wall@jetstack.io>

--- internal/apis/certmanager/v1alpha3/register.go
+++ internal/apis/certmanager/v1alpha3/register.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1alpha3
 
 import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 
@@ -42,4 +43,25 @@ func init() {
 	// generated functions takes place in the generated files. The separation
 	// makes the code compile even when the generated files are missing.
 	localSchemeBuilder.Register(addDefaultingFuncs)
+
+	// We only register manually written functions here. The registration of the
+	// generated functions takes place in the generated files. The separation
+	// makes the code compile even when the generated files are missing.
+	localSchemeBuilder.Register(addKnownTypes)
+}
+
+// Adds the list of known types to api.Scheme.
+func addKnownTypes(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(SchemeGroupVersion,
+		&Certificate{},
+		&CertificateList{},
+		&Issuer{},
+		&IssuerList{},
+		&ClusterIssuer{},
+		&ClusterIssuerList{},
+		&CertificateRequest{},
+		&CertificateRequestList{},
+	)
+	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
+	return nil
 }
