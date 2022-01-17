b6d398e3a4bdccdbe782164303f443c3d3cf906e
Maël Valais <mael@vls.dev>
2022-01-17 18:38:11 +0100

    trigger controller: unit test for the "RequestMatchesSpec" func
    Signed-off-by: Maël Valais <mael@vls.dev>

--- pkg/controller/certificates/util_test.go
+++ pkg/controller/certificates/util_test.go
@@ -23,12 +23,18 @@ import (
 	"testing"
 	"time"
 
+	"github.com/davecgh/go-spew/spew"
+	"github.com/pmezard/go-difflib/difflib"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/pointer"
 
 	cmapi "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
+	cmmeta "github.com/cert-manager/cert-manager/pkg/apis/meta/v1"
 	"github.com/cert-manager/cert-manager/pkg/util/pki"
+	testcrypto "github.com/cert-manager/cert-manager/test/unit/crypto"
 )
 
 func mustGenerateRSA(t *testing.T, keySize int) crypto.PrivateKey {
@@ -360,4 +366,156 @@ func TestRenewalTime(t *testing.T) {
 
 		})
 	}
+}
+
+func TestRequestMatchesSpec(t *testing.T) {
+	type Cert cmapi.Certificate // Those long types are making the lines go wee!
+	staticFixedPrivateKey := testcrypto.MustCreatePEMPrivateKey(t)
+
+	originalCert := &cmapi.Certificate{Spec: cmapi.CertificateSpec{
+		CommonName: "does-not-matter.example.com",
+		Subject: &cmapi.X509Subject{
+			Organizations:       []string{"org1", "org2"},
+			Countries:           []string{"us"},
+			OrganizationalUnits: []string{"ou1", "ou2"},
+			Localities:          []string{"loc1", "loc2"},
+			Provinces:           []string{"prov1", "prov2"},
+			StreetAddresses:     []string{"street1", "street2"},
+			PostalCodes:         []string{"post1", "post2"},
+			SerialNumber:        "12345",
+		},
+		Duration:       &metav1.Duration{Duration: 30 * 24 * time.Hour},
+		RenewBefore:    &metav1.Duration{Duration: 90 * 24 * time.Hour},
+		DNSNames:       []string{"example.com"},
+		IPAddresses:    []string{"1.2.3.4"},
+		URIs:           []string{"http://example.com"},
+		EmailAddresses: []string{"foo@bar.com"},
+		SecretName:     "does-not-matter",
+		SecretTemplate: &cmapi.CertificateSecretTemplate{
+			Labels: map[string]string{"foo": "bar"},
+		},
+		Keystores: &cmapi.CertificateKeystores{
+			JKS: &cmapi.JKSKeystore{
+				Create: true,
+				PasswordSecretRef: cmmeta.SecretKeySelector{
+					Key:                  "password",
+					LocalObjectReference: cmmeta.LocalObjectReference{Name: "foo"},
+				},
+			},
+			PKCS12: &cmapi.PKCS12Keystore{
+				Create: true,
+				PasswordSecretRef: cmmeta.SecretKeySelector{
+					Key:                  "password",
+					LocalObjectReference: cmmeta.LocalObjectReference{Name: "foo"},
+				},
+			},
+		},
+		IssuerRef: cmmeta.ObjectReference{
+			Name:  "testissuer",
+			Kind:  "IssuerKind",
+			Group: "group.example.com",
+		},
+		IsCA: true,
+		Usages: []cmapi.KeyUsage{
+			cmapi.UsageSigning,
+			cmapi.UsageDigitalSignature,
+			cmapi.UsageKeyEncipherment,
+		},
+		PrivateKey: &cmapi.CertificatePrivateKey{
+			Algorithm:      cmapi.RSAKeyAlgorithm,
+			Size:           2048,
+			RotationPolicy: cmapi.RotationPolicyNever,
+			Encoding:       cmapi.PKCS8,
+		},
+		EncodeUsagesInRequest: pointer.Bool(true),
+		RevisionHistoryLimit:  pointer.Int32(2),
+	}}
+	originalCR := &cmapi.CertificateRequest{Spec: cmapi.CertificateRequestSpec{
+		IssuerRef: cmmeta.ObjectReference{
+			Name:  "testissuer",
+			Kind:  "IssuerKind",
+			Group: "group.example.com",
+		},
+		Request: testcrypto.MustGenerateCSRImpl(t, staticFixedPrivateKey, originalCert),
+		IsCA:    true,
+		Usages: []cmapi.KeyUsage{
+			cmapi.UsageSigning,
+			cmapi.UsageDigitalSignature,
+			cmapi.UsageKeyEncipherment,
+		},
+		Duration: &metav1.Duration{Duration: 30 * 24 * time.Hour},
+	}}
+
+	tests := []struct {
+		change     func(c *Cert) // Nil if no change is to be made.
+		mismatches []string
+	}{
+		// Happy case: with no change to the Certificate, no mismatch is
+		// expected.
+		{change: nil, mismatches: nil},
+
+		// The following Certificate fields are looked up by the
+		// RequestMatchesSpec function.
+		{change: func(c *Cert) { c.Spec.CommonName = "changed" }, mismatches: []string{"spec.commonName"}},
+		{change: func(c *Cert) { c.Spec.DNSNames = []string{"changed"} }, mismatches: []string{"spec.dnsNames"}},
+		{change: func(c *Cert) { c.Spec.IPAddresses = []string{"4.3.2.1"} }, mismatches: []string{"spec.ipAddresses"}},
+		{change: func(c *Cert) { c.Spec.URIs = []string{"https://changed"} }, mismatches: []string{"spec.uris"}},
+		{change: func(c *Cert) { c.Spec.EmailAddresses = []string{"changed@bar.com"} }, mismatches: []string{"spec.emailAddresses"}},
+		{change: func(c *Cert) { c.Spec.Usages = []cmapi.KeyUsage{cmapi.UsageAny} }, mismatches: []string{"spec.usages"}},
+		{change: func(c *Cert) { c.Spec.IsCA = false }, mismatches: []string{"spec.isCA"}},
+		{change: func(c *Cert) { c.Spec.Duration = &metav1.Duration{Duration: 1 * time.Hour} }, mismatches: []string{"spec.duration"}},
+		{change: func(c *Cert) { c.Spec.IssuerRef = cmmeta.ObjectReference{Name: "changed"} }, mismatches: []string{"spec.issuerRef"}},
+
+		// The following Certificate fields are not looked at by the
+		// RequestMatchesSpec function.
+		{change: func(c *Cert) { c.Spec.PrivateKey.Algorithm = cmapi.ECDSAKeyAlgorithm }, mismatches: nil},
+		{change: func(c *Cert) { c.Spec.PrivateKey.Size = 4096 }, mismatches: nil},
+		{change: func(c *Cert) { c.Spec.EncodeUsagesInRequest = pointer.Bool(false) }, mismatches: nil},
+		{change: func(c *Cert) { c.Spec.Keystores = &cmapi.CertificateKeystores{} }, mismatches: nil},
+		{change: func(c *Cert) { c.Spec.RenewBefore = &metav1.Duration{Duration: 1 * time.Hour} }, mismatches: nil},
+		{change: func(c *Cert) { c.Spec.RevisionHistoryLimit = pointer.Int32(10) }, mismatches: nil},
+		{change: func(c *Cert) { c.Spec.SecretName = "changed" }, mismatches: nil}, // (1)
+		{change: func(c *Cert) { c.Spec.SecretTemplate = &cmapi.CertificateSecretTemplate{} }, mismatches: nil},
+		{change: func(c *Cert) { c.Spec.PrivateKey.Encoding = cmapi.PKCS1 }, mismatches: nil},
+		{change: func(c *Cert) { c.Spec.PrivateKey.RotationPolicy = cmapi.RotationPolicyAlways }, mismatches: nil},
+	}
+
+	for _, test := range tests {
+		t.Run("", func(t *testing.T) {
+			cert := originalCert.DeepCopy()
+			var diffCert string
+			if test.change != nil {
+				test.change((*Cert)(cert))
+				diffCert, _ := difflib.GetUnifiedDiffString(difflib.UnifiedDiff{
+					A:       difflib.SplitLines(spewConf.Sdump(originalCert)),
+					B:       difflib.SplitLines(spewConf.Sdump(cert)),
+					Context: 1,
+				})
+				if test.change != nil && diffCert == "" {
+					t.Fatal("incorrect test case: the func to change the Certificate is non-nil but no change has been detected on the Certificate")
+				}
+			}
+			gotMismatches, gotErr := RequestMatchesSpec(originalCR, cert.Spec)
+			require.NoError(t, gotErr)
+
+			if !reflect.DeepEqual(test.mismatches, gotMismatches) {
+				debug := ""
+				if diffCert == "" {
+					debug += "with no change to the Certificate"
+				} else {
+					debug += fmt.Sprintf("with the following changes to the Certificate : %s", diffCert)
+				}
+				t.Errorf("%s, expected mismatches=%v but got mismatches=%v", debug, test.mismatches, gotMismatches)
+			}
+		})
+	}
+}
+
+var spewConf = spew.ConfigState{
+	Indent:                  " ",
+	DisablePointerAddresses: true,
+	DisableCapacities:       true,
+	SortKeys:                true,
+	DisableMethods:          true,
+	MaxDepth:                10,
 }
