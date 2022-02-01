d5365af66222ff2afeb8229f82e5c9b01fdc488d
joshvanl <vleeuwenjoshua@gmail.com>
2022-02-01 15:34:37 +0000

    Adds e2e tests for additional output formats
    Signed-off-by: joshvanl <vleeuwenjoshua@gmail.com>

--- test/e2e/suite/doc.go
+++ test/e2e/suite/doc.go
@@ -18,8 +18,8 @@ package suite
 
 import (
 	_ "github.com/cert-manager/cert-manager/test/e2e/suite/approval"
+	_ "github.com/cert-manager/cert-manager/test/e2e/suite/certificates"
 	_ "github.com/cert-manager/cert-manager/test/e2e/suite/conformance"
 	_ "github.com/cert-manager/cert-manager/test/e2e/suite/issuers"
-	_ "github.com/cert-manager/cert-manager/test/e2e/suite/secrettemplate"
 	_ "github.com/cert-manager/cert-manager/test/e2e/suite/serving"
 )
