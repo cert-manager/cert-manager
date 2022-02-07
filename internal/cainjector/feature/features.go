23603775e1a020e5d059c11074856c16016b6f38
joshvanl <vleeuwenjoshua@gmail.com>
2022-02-07 15:05:06 +0000

    Change import `jetstack/cert-manager` -> `cert-manager/cert-manager`
    Signed-off-by: joshvanl <vleeuwenjoshua@gmail.com>

--- internal/cainjector/feature/features.go
+++ internal/cainjector/feature/features.go
@@ -19,7 +19,7 @@ package feature
 import (
 	"k8s.io/component-base/featuregate"
 
-	utilfeature "github.com/jetstack/cert-manager/pkg/util/feature"
+	utilfeature "github.com/cert-manager/cert-manager/pkg/util/feature"
 )
 
 const (
@@ -40,5 +40,5 @@ func init() {
 // To add a new feature, define a key for it above and add it here.
 // To check whether a feature is enabled, use:
 //   utilfeature.DefaultFeatureGate.Enabled(feature.FeatureName)
-// Where utilfeature is github.com/jetstack/cert-manager/pkg/util/feature.
+// Where utilfeature is github.com/cert-manager/cert-manager/pkg/util/feature.
 var cainjectorFeatureGates = map[featuregate.Feature]featuregate.FeatureSpec{}
