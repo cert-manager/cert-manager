aa456b9c3ff21360897a2448109d12a87130ab2d
joshvanl <vleeuwenjoshua@gmail.com>
2022-02-11 16:52:12 +0000

    Adds roundtrip tests to challenge apply serializer
    Signed-off-by: joshvanl <vleeuwenjoshua@gmail.com>

--- internal/controller/challenges/apply_test.go
+++ internal/controller/challenges/apply_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 The cert-manager Authors.
+Copyright 2022 The cert-manager Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -17,12 +17,14 @@ limitations under the License.
 package challenges
 
 import (
+	"encoding/json"
 	"strconv"
 	"sync"
 	"testing"
 
 	fuzz "github.com/google/gofuzz"
 	"github.com/stretchr/testify/assert"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 
 	cmacme "github.com/cert-manager/cert-manager/pkg/apis/acme/v1"
 )
@@ -45,7 +47,8 @@ func Test_serializeApply(t *testing.T) {
 					fuzz.New().NilChance(0.5).Funcs(
 						func(challenge *cmacme.Challenge, c fuzz.Continue) {
 							if challenge.Spec.Solver.DNS01 != nil && challenge.Spec.Solver.DNS01.Webhook != nil {
-								challenge.Spec.Solver.DNS01.Webhook.Config = nil
+								// Config can only hold data which originates from proper JSON.
+								challenge.Spec.Solver.DNS01.Webhook.Config = &apiextensionsv1.JSON{Raw: []byte(`{"some": {"json": "test"}, "string": 42}`)}
 							}
 						},
 					).Fuzz(&challenge)
@@ -54,6 +57,11 @@ func Test_serializeApply(t *testing.T) {
 					challengeData, err := serializeApply(&challenge)
 					assert.NoError(t, err, "%+#v", challenge)
 					assert.Regexp(t, expReg, string(challengeData))
+
+					// Test a roundtrip results in the same data.
+					var rtChallenge cmacme.Challenge
+					assert.NoError(t, json.Unmarshal(challengeData, &rtChallenge))
+					assert.Equal(t, challenge.Spec, rtChallenge.Spec)
 
 					wg.Done()
 				})
@@ -98,6 +106,11 @@ func Test_serializeApplyStatus(t *testing.T) {
 					challengeData, err = serializeApplyStatus(&challenge)
 					assert.NoError(t, err)
 					assert.Equal(t, expEmpty, string(challengeData))
+
+					// Test a roundtrip results in the same data.
+					var rtChallenge cmacme.Challenge
+					assert.NoError(t, json.Unmarshal(challengeData, &rtChallenge))
+					assert.Equal(t, challenge.Status, rtChallenge.Status)
 
 					wg.Done()
 				})
