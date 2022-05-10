6cb826833eb79ab0ce930ea8a4138b2a0994df3a
irbekrm <irbekrm@gmail.com>
2022-05-10 11:32:39 +0100

    Ensures Envoy ClusterIP is parameterized consistently
    Signed-off-by: irbekrm <irbekrm@gmail.com>

--- devel/lib/lib.sh
+++ devel/lib/lib.sh
@@ -34,6 +34,7 @@ export OPENSHIFT_VERSION="${OPENSHIFT_VERSION:-"3.11"}"
 export SERVICE_IP_PREFIX="${SERVICE_IP_PREFIX:-10.0.0}"
 export DNS_SERVER="${SERVICE_IP_PREFIX}.16"
 export INGRESS_IP="${SERVICE_IP_PREFIX}.15"
+export GATEWAY_IP="${SERVICE_IP_PREFIX}.14"
 
 # setup_tools will build and set up the environment to use bazel-provided
 # versions of the tools required for development
