8b2dc2a7465852c6668d0647ca8f0549007bac7e
joshvanl <me@joshvanl.dev>
2022-08-09 11:25:41 +0100

    Update ginkgo import path to use /v2
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- test/e2e/suite/certificaterequests/selfsigned/secret.go
+++ test/e2e/suite/certificaterequests/selfsigned/secret.go
@@ -19,7 +19,7 @@ package selfsigned
 import (
 	"context"
 
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
