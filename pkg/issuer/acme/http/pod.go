954eb0d875cb79d6da8eca233ccc9267ebf1420c
Richard Wall <richard.wall@jetstack.io>
2023-01-24 17:00:11 +0000

    automount service account tokens off by default
    Signed-off-by: Richard Wall <richard.wall@jetstack.io>

--- pkg/issuer/acme/http/pod.go
+++ pkg/issuer/acme/http/pod.go
@@ -175,6 +175,7 @@ func (s *Solver) buildDefaultPod(ch *cmacme.Challenge) *corev1.Pod {
 			OwnerReferences: []metav1.OwnerReference{*metav1.NewControllerRef(ch, challengeGvk)},
 		},
 		Spec: corev1.PodSpec{
+			AutomountServiceAccountToken: pointer.Bool(false),
 			NodeSelector: map[string]string{
 				"kubernetes.io/os": "linux",
 			},
