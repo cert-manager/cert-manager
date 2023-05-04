a45a8b3a39d32f703d83354c89cec7e8a0f07a67
irbekrm <irbekrm@gmail.com>
2023-05-04 12:53:50 +0100

    Adds a package comment, fixes imports
    Signed-off-by: irbekrm <irbekrm@gmail.com>

--- test/acme/util.go
+++ test/acme/util.go
@@ -14,6 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// package dns contains a framework for testing ACME DNS solver implementations.
+// Used by both internal and external solvers.
 package dns
 
 import (
