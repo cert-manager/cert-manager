9d2d1cd6eff519de0f945d5fd907a76cbd89d2b1
Tim Ramlot <42113979+inteon@users.noreply.github.com>
2023-08-23 14:12:51 +0200

    add openapi definitions to acme API server
    Signed-off-by: Tim Ramlot <42113979+inteon@users.noreply.github.com>

--- pkg/acme/webhook/apiserver/apiserver.go
+++ pkg/acme/webhook/apiserver/apiserver.go
@@ -25,12 +25,14 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/apimachinery/pkg/version"
+	"k8s.io/apiserver/pkg/endpoints/openapi"
 	"k8s.io/apiserver/pkg/registry/rest"
 	genericapiserver "k8s.io/apiserver/pkg/server"
 	restclient "k8s.io/client-go/rest"
 
 	"github.com/cert-manager/cert-manager/pkg/acme/webhook"
 	whapi "github.com/cert-manager/cert-manager/pkg/acme/webhook/apis/acme/v1alpha1"
+	cmopenapi "github.com/cert-manager/cert-manager/pkg/acme/webhook/openapi"
 	"github.com/cert-manager/cert-manager/pkg/acme/webhook/registry/challengepayload"
 )
 
@@ -54,20 +56,12 @@ func init() {
 		&metav1.APIGroupList{},
 		&metav1.APIGroup{},
 		&metav1.APIResourceList{},
-		&metav1.ListOptions{},
-		&metav1.GetOptions{},
-		&metav1.PatchOptions{},
-		&metav1.DeleteOptions{},
-		&metav1.CreateOptions{},
-		&metav1.UpdateOptions{},
 	)
 }
 
 type Config struct {
 	GenericConfig *genericapiserver.RecommendedConfig
 	ExtraConfig   ExtraConfig
-
-	restConfig *restclient.Config
 }
 
 type ExtraConfig struct {
@@ -101,13 +95,16 @@ func (c *Config) Complete() CompletedConfig {
 	completedCfg := completedConfig{
 		c.GenericConfig.Complete(),
 		&c.ExtraConfig,
-		c.restConfig,
+		c.GenericConfig.ClientConfig,
 	}
 
 	completedCfg.GenericConfig.Version = &version.Info{
 		Major: "1",
 		Minor: "1",
 	}
+
+	completedCfg.GenericConfig.OpenAPIConfig = genericapiserver.DefaultOpenAPIConfig(cmopenapi.GetOpenAPIDefinitions, openapi.NewDefinitionNamer(Scheme))
+	completedCfg.GenericConfig.OpenAPIV3Config = genericapiserver.DefaultOpenAPIV3Config(cmopenapi.GetOpenAPIDefinitions, openapi.NewDefinitionNamer(Scheme))
 
 	return CompletedConfig{&completedCfg}
 }
@@ -126,42 +123,35 @@ func (c completedConfig) New() (*ChallengeServer, error) {
 		GenericAPIServer: genericServer,
 	}
 
-	if c.restConfig == nil {
-		c.restConfig, err = restclient.InClusterConfig()
-		if err != nil {
-			return nil, err
-		}
-	}
-
 	// TODO we're going to need a later k8s.io/apiserver so that we can get discovery to list a different group version for
 	// our endpoint which we'll use to back some custom storage which will consume the AdmissionReview type and give back the correct response
 	apiGroupInfo := genericapiserver.APIGroupInfo{
 		VersionedResourcesStorageMap: map[string]map[string]rest.Storage{},
-		// TODO unhardcode this. It was hardcoded before, but we need to re-evaluate
-		OptionsExternalVersion: &schema.GroupVersion{Version: "v1alpha1"},
+		// TODO unhardcode this.  It was hardcoded before, but we need to re-evaluate
+		OptionsExternalVersion: &schema.GroupVersion{Version: "v1"},
 		Scheme:                 Scheme,
 		ParameterCodec:         metav1.ParameterCodec,
 		NegotiatedSerializer:   Codecs,
 	}
 
 	for _, solver := range solversByName(c.ExtraConfig.Solvers...) {
-		challengeHandler := challengepayload.NewREST(solver)
-		v1alpha1storage, ok := apiGroupInfo.VersionedResourcesStorageMap["v1alpha1"]
-		if !ok {
-			v1alpha1storage = map[string]rest.Storage{}
-		}
-
 		gvr := metav1.GroupVersionResource{
 			Group:    c.ExtraConfig.SolverGroup,
 			Version:  "v1alpha1",
 			Resource: solver.Name(),
 		}
 
+		challengeHandler := challengepayload.NewREST(solver)
+
 		apiGroupInfo.PrioritizedVersions = appendUniqueGroupVersion(apiGroupInfo.PrioritizedVersions, schema.GroupVersion{
 			Group:   gvr.Group,
 			Version: gvr.Version,
 		})
 
+		v1alpha1storage, ok := apiGroupInfo.VersionedResourcesStorageMap[gvr.Version]
+		if !ok {
+			v1alpha1storage = map[string]rest.Storage{}
+		}
 		v1alpha1storage[gvr.Resource] = challengeHandler
 		apiGroupInfo.VersionedResourcesStorageMap[gvr.Version] = v1alpha1storage
 	}
