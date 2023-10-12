ad3bc2c66ad8f854b2394279770a3d979cfdc2b3
Ashley Davis <ashley.davis@venafi.com>
2023-10-12 10:27:16 +0100

    bump go to latest version to address CVE-2023-39325
    Signed-off-by: Ashley Davis <ashley.davis@venafi.com>

--- make/tools.mk
+++ make/tools.mk
@@ -73,7 +73,7 @@ KUBEBUILDER_ASSETS_VERSION=1.28.0
 TOOLS += etcd=$(KUBEBUILDER_ASSETS_VERSION)
 TOOLS += kube-apiserver=$(KUBEBUILDER_ASSETS_VERSION)
 
-VENDORED_GO_VERSION := 1.21.1
+VENDORED_GO_VERSION := 1.21.3
 
 # When switching branches which use different versions of the tools, we
 # need a way to re-trigger the symlinking from $(BINDIR)/downloaded to $(BINDIR)/tools.
