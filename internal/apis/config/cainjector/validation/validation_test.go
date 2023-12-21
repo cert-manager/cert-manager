59184de02fb68f2804eb874eebd95ab3d7fd7d8c
Adam Talbot <adam.talbot@venafi.com>
2023-12-21 08:45:19 +0000

    test: add tests for config validation functions
    Signed-off-by: Adam Talbot <adam.talbot@venafi.com>

---
+++ internal/apis/config/cainjector/validation/validation_test.go
@@ -0,0 +1,40 @@
+/*
+Copyright 2021 The cert-manager Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package validation
+
+import (
+	"testing"
+
+	config "github.com/cert-manager/cert-manager/internal/apis/config/cainjector"
+)
+
+func TestValidateCAInjectorConfiguration(t *testing.T) {
+	tests := []struct {
+		name    string
+		config  *config.CAInjectorConfiguration
+		wantErr bool
+	}{
+		// TODO: Add test cases once validation function padded out.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := ValidateCAInjectorConfiguration(tt.config); (err != nil) != tt.wantErr {
+				t.Errorf("ValidateCAInjectorConfiguration() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
