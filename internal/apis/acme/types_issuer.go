9f7edc49cbfc24363f2a00f7432afe36746f3612
Adrian Lai <adrian.lai@jetstack.io>
2023-12-28 15:42:22 +0000

    Remove protobuf annotations
    These were copy-pasted in from the parent definitions. We don't marshal
    to protobuf (none of the other structs have equivalent annotations), so
    remove them as they are unnecessary.
    
    Signed-off-by: Adrian Lai <adrian.lai@jetstack.io>

--- internal/apis/acme/types_issuer.go
+++ internal/apis/acme/types_issuer.go
@@ -373,7 +373,7 @@ type ACMEChallengeSolverHTTP01IngressPodSecurityContext struct {
 	// takes precedence for that container.
 	// Note that this field cannot be set when spec.os.name is windows.
 	// +optional
-	SELinuxOptions *corev1.SELinuxOptions `json:"seLinuxOptions,omitempty" protobuf:"bytes,1,opt,name=seLinuxOptions"`
+	SELinuxOptions *corev1.SELinuxOptions `json:"seLinuxOptions,omitempty"`
 	// The UID to run the entrypoint of the container process.
 	// Defaults to user specified in image metadata if unspecified.
 	// May also be set in SecurityContext.  If set in both SecurityContext and
@@ -381,7 +381,7 @@ type ACMEChallengeSolverHTTP01IngressPodSecurityContext struct {
 	// for that container.
 	// Note that this field cannot be set when spec.os.name is windows.
 	// +optional
-	RunAsUser *int64 `json:"runAsUser,omitempty" protobuf:"varint,2,opt,name=runAsUser"`
+	RunAsUser *int64 `json:"runAsUser,omitempty"`
 	// The GID to run the entrypoint of the container process.
 	// Uses runtime default if unset.
 	// May also be set in SecurityContext.  If set in both SecurityContext and
@@ -389,7 +389,7 @@ type ACMEChallengeSolverHTTP01IngressPodSecurityContext struct {
 	// for that container.
 	// Note that this field cannot be set when spec.os.name is windows.
 	// +optional
-	RunAsGroup *int64 `json:"runAsGroup,omitempty" protobuf:"varint,6,opt,name=runAsGroup"`
+	RunAsGroup *int64 `json:"runAsGroup,omitempty"`
 	// Indicates that the container must run as a non-root user.
 	// If true, the Kubelet will validate the image at runtime to ensure that it
 	// does not run as UID 0 (root) and fail to start the container if it does.
@@ -397,7 +397,7 @@ type ACMEChallengeSolverHTTP01IngressPodSecurityContext struct {
 	// May also be set in SecurityContext.  If set in both SecurityContext and
 	// PodSecurityContext, the value specified in SecurityContext takes precedence.
 	// +optional
-	RunAsNonRoot *bool `json:"runAsNonRoot,omitempty" protobuf:"varint,3,opt,name=runAsNonRoot"`
+	RunAsNonRoot *bool `json:"runAsNonRoot,omitempty"`
 	// A list of groups applied to the first process run in each container, in addition
 	// to the container's primary GID, the fsGroup (if specified), and group memberships
 	// defined in the container image for the uid of the container process. If unspecified,
@@ -406,7 +406,7 @@ type ACMEChallengeSolverHTTP01IngressPodSecurityContext struct {
 	// even if they are not included in this list.
 	// Note that this field cannot be set when spec.os.name is windows.
 	// +optional
-	SupplementalGroups []int64 `json:"supplementalGroups,omitempty" protobuf:"varint,4,rep,name=supplementalGroups"`
+	SupplementalGroups []int64 `json:"supplementalGroups,omitempty"`
 	// A special supplemental group that applies to all containers in a pod.
 	// Some volume types allow the Kubelet to change the ownership of that volume
 	// to be owned by the pod:
@@ -418,12 +418,12 @@ type ACMEChallengeSolverHTTP01IngressPodSecurityContext struct {
 	// If unset, the Kubelet will not modify the ownership and permissions of any volume.
 	// Note that this field cannot be set when spec.os.name is windows.
 	// +optional
-	FSGroup *int64 `json:"fsGroup,omitempty" protobuf:"varint,5,opt,name=fsGroup"`
+	FSGroup *int64 `json:"fsGroup,omitempty"`
 	// Sysctls hold a list of namespaced sysctls used for the pod. Pods with unsupported
 	// sysctls (by the container runtime) might fail to launch.
 	// Note that this field cannot be set when spec.os.name is windows.
 	// +optional
-	Sysctls []corev1.Sysctl `json:"sysctls,omitempty" protobuf:"bytes,7,rep,name=sysctls"`
+	Sysctls []corev1.Sysctl `json:"sysctls,omitempty"`
 	// fsGroupChangePolicy defines behavior of changing ownership and permission of the volume
 	// before being exposed inside Pod. This field will only apply to
 	// volume types which support fsGroup based ownership(and permissions).
@@ -432,11 +432,11 @@ type ACMEChallengeSolverHTTP01IngressPodSecurityContext struct {
 	// Valid values are "OnRootMismatch" and "Always". If not specified, "Always" is used.
 	// Note that this field cannot be set when spec.os.name is windows.
 	// +optional
-	FSGroupChangePolicy *corev1.PodFSGroupChangePolicy `json:"fsGroupChangePolicy,omitempty" protobuf:"bytes,9,opt,name=fsGroupChangePolicy"`
+	FSGroupChangePolicy *corev1.PodFSGroupChangePolicy `json:"fsGroupChangePolicy,omitempty"`
 	// The seccomp options to use by the containers in this pod.
 	// Note that this field cannot be set when spec.os.name is windows.
 	// +optional
-	SeccompProfile *corev1.SeccompProfile `json:"seccompProfile,omitempty" protobuf:"bytes,10,opt,name=seccompProfile"`
+	SeccompProfile *corev1.SeccompProfile `json:"seccompProfile,omitempty"`
 }
 
 // CNAMEStrategy configures how the DNS01 provider should handle CNAME records
