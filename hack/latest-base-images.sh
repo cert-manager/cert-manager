4f848bf2eed18ea7b2dbee19659efa89e0e75208
Tim Ramlot <42113979+inteon@users.noreply.github.com>
2024-01-02 13:26:21 +0100

    upgrade base image from debian11 to debian12
    Signed-off-by: Tim Ramlot <42113979+inteon@users.noreply.github.com>

--- hack/latest-base-images.sh
+++ hack/latest-base-images.sh
@@ -26,8 +26,8 @@ CRANE=crane
 
 TARGET=make/base_images.mk
 
-STATIC_BASE=gcr.io/distroless/static-debian11
-DYNAMIC_BASE=gcr.io/distroless/base-debian11
+STATIC_BASE=gcr.io/distroless/static-debian12
+DYNAMIC_BASE=gcr.io/distroless/base-debian12
 
 mkdir -p make
 
