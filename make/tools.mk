9570427dddec940ea7374022013460f417a9de67
Ashley Davis <ashley.davis@venafi.com>
2024-04-22 14:49:35 +0100

    [release-1.13] fix CVE-2023-45288, bump base images, bump go
    Signed-off-by: Ashley Davis <ashley.davis@venafi.com>

--- make/tools.mk
+++ make/tools.mk
@@ -75,7 +75,7 @@ KUBEBUILDER_ASSETS_VERSION=1.28.0
 TOOLS += etcd=$(KUBEBUILDER_ASSETS_VERSION)
 TOOLS += kube-apiserver=$(KUBEBUILDER_ASSETS_VERSION)
 
-VENDORED_GO_VERSION := 1.21.8
+VENDORED_GO_VERSION := 1.21.9
 
 # When switching branches which use different versions of the tools, we
 # need a way to re-trigger the symlinking from $(BINDIR)/downloaded to $(BINDIR)/tools.
