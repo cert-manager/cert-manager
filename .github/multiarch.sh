90a6c35c03e284ae55a8242f83c85ca3cdd91205
psbrar99 <brargg1989@gmail.com>
2024-06-16 12:52:21 -0600

    add script
    Signed-off-by: psbrar99 <brargg1989@gmail.com>
    (cherry picked from commit ec7e58fc5df3bd0682d3fff9459e37fdb578312b)

---
+++ .github/multiarch.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+# set -x
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
+IMAGES=(cert-manager-acmesolver
+cert-manager-startupapicheck
+cert-manager-webhook
+cert-manager-controller
+)
+
+if [[ ${TAG} =~ "fips" ]]; then
+    echo "pushing cert-manager images from amd64 only"
+    for image in "${IMAGES[@]}"; do
+         docker tag  $image-amd64:$TAG $HUB/$image-amd64:$TAG  
+         docker push  $HUB/$image-amd64:$TAG         
+         docker manifest create $HUB/$image:$TAG --amend $HUB/$image-amd64:$TAG
+         docker manifest push $HUB/$image:$TAG
+    done
+else
+    for image in "${IMAGES[@]}"; do
+         docker tag  $image-amd64:$TAG $HUB/$image-amd64:$TAG
+         docker tag  $image-arm64:$TAG $HUB/$image-arm64:$TAG
+         docker push   $HUB/$image-amd64:$TAG
+         docker push   $HUB/$image-arm64:$TAG
+         docker manifest create $HUB/$image:$TAG --amend $HUB/$image-amd64:$TAG --amend $HUB/$image-arm64:$TAG
+         docker manifest push $HUB/$image:$TAG
+    done
+fi
+   
+
