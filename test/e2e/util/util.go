461ff389ed7d366573602735cd1b1d2809e02689
Erik Godding Boye <egboye@gmail.com>
2025-08-31 22:14:55 +0200

    Fix misleading gateway-api import alias
    Signed-off-by: Erik Godding Boye <egboye@gmail.com>

--- test/e2e/util/util.go
+++ test/e2e/util/util.go
@@ -40,7 +40,7 @@ import (
 	"k8s.io/client-go/discovery"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
-	gwapiv1beta1 "sigs.k8s.io/gateway-api/apis/v1"
+	gwapi "sigs.k8s.io/gateway-api/apis/v1"
 
 	"github.com/cert-manager/cert-manager/e2e-tests/framework/log"
 	apiutil "github.com/cert-manager/cert-manager/pkg/api/util"
@@ -312,19 +312,19 @@ func pathTypePrefix() *networkingv1.PathType {
 // watching the 'foo' gateway class, so this Gateway will not be used to
 // actually route traffic, but can be used to test cert-manager controllers that
 // sync Gateways, such as gateway-shim.
-func NewGateway(gatewayName, ns, secretName string, annotations map[string]string, dnsNames ...string) *gwapiv1beta1.Gateway {
+func NewGateway(gatewayName, ns, secretName string, annotations map[string]string, dnsNames ...string) *gwapi.Gateway {
 
-	return &gwapiv1beta1.Gateway{
+	return &gwapi.Gateway{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:        gatewayName,
 			Annotations: annotations,
 		},
-		Spec: gwapiv1beta1.GatewaySpec{
+		Spec: gwapi.GatewaySpec{
 			GatewayClassName: "foo",
-			Listeners: []gwapiv1beta1.Listener{{
-				AllowedRoutes: &gwapiv1beta1.AllowedRoutes{
-					Namespaces: &gwapiv1beta1.RouteNamespaces{
-						From: func() *gwapiv1beta1.FromNamespaces { f := gwapiv1beta1.NamespacesFromSame; return &f }(),
+			Listeners: []gwapi.Listener{{
+				AllowedRoutes: &gwapi.AllowedRoutes{
+					Namespaces: &gwapi.RouteNamespaces{
+						From: func() *gwapi.FromNamespaces { f := gwapi.NamespacesFromSame; return &f }(),
 						Selector: &metav1.LabelSelector{MatchLabels: map[string]string{
 							"gw": gatewayName,
 						}},
@@ -332,16 +332,16 @@ func NewGateway(gatewayName, ns, secretName string, annotations map[string]strin
 					Kinds: nil,
 				},
 				Name:     "acme-solver",
-				Protocol: gwapiv1beta1.TLSProtocolType,
-				Port:     gwapiv1beta1.PortNumber(443),
-				Hostname: (*gwapiv1beta1.Hostname)(&dnsNames[0]),
-				TLS: &gwapiv1beta1.GatewayTLSConfig{
-					CertificateRefs: []gwapiv1beta1.SecretObjectReference{
+				Protocol: gwapi.TLSProtocolType,
+				Port:     gwapi.PortNumber(443),
+				Hostname: (*gwapi.Hostname)(&dnsNames[0]),
+				TLS: &gwapi.GatewayTLSConfig{
+					CertificateRefs: []gwapi.SecretObjectReference{
 						{
-							Kind:      func() *gwapiv1beta1.Kind { k := gwapiv1beta1.Kind("Secret"); return &k }(),
-							Name:      gwapiv1beta1.ObjectName(secretName),
-							Group:     func() *gwapiv1beta1.Group { g := gwapiv1beta1.Group(corev1.GroupName); return &g }(),
-							Namespace: (*gwapiv1beta1.Namespace)(&ns),
+							Kind:      func() *gwapi.Kind { k := gwapi.Kind("Secret"); return &k }(),
+							Name:      gwapi.ObjectName(secretName),
+							Group:     func() *gwapi.Group { g := gwapi.Group(corev1.GroupName); return &g }(),
+							Namespace: (*gwapi.Namespace)(&ns),
 						},
 					},
 				},
