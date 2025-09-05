d35713b750c1cabce640645e6b5435ae2cfac6f7
Erik Godding Boye <egboye@gmail.com>
2025-09-05 20:56:36 +0200

    No c/r leader election labels
    Signed-off-by: Erik Godding Boye <egboye@gmail.com>

--- cmd/cainjector/app/controller.go
+++ cmd/cainjector/app/controller.go
@@ -141,7 +141,6 @@ func Run(opts *config.CAInjectorConfiguration, ctx context.Context) error {
 			LeaderElectionNamespace:       opts.LeaderElectionConfig.Namespace,
 			LeaderElectionID:              "cert-manager-cainjector-leader-election",
 			LeaderElectionReleaseOnCancel: true,
-			LeaderElectionLabels:          map[string]string{"app.kubernetes.io/managed-by": "cert-manager", "app.kubernetes.io/name": "ca-injector"},
 			LeaderElectionResourceLock:    resourcelock.LeasesResourceLock,
 			LeaseDuration:                 &opts.LeaderElectionConfig.LeaseDuration,
 			RenewDeadline:                 &opts.LeaderElectionConfig.RenewDeadline,
