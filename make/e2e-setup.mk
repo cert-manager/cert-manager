635387ca4d661102c327b97d5497c46bc79fa860
Hemant Joshi <mail@hjoshi.me>
2026-01-21 10:19:57 -0700

    fix(e2e): replacing contour with kgateway (#8426)
    * replacing contour with kgateway
    
    Signed-off-by: hjoshi123 <mail@hjoshi.me>
    Signed-off-by: Hemant Joshi <mail@hjoshi.me>
    
    * reverting kyverno changes fmt
    
    Signed-off-by: Hemant Joshi <mail@hjoshi.me>
    
    ---------
    
    Signed-off-by: hjoshi123 <mail@hjoshi.me>
    Signed-off-by: Hemant Joshi <mail@hjoshi.me>

--- make/e2e-setup.mk
+++ make/e2e-setup.mk
@@ -32,11 +32,7 @@ IMAGE_kyvernopre_amd64 := reg.kyverno.io/kyverno/kyvernopre:v1.16.2@sha256:1d133
 IMAGE_vault_amd64 := docker.io/hashicorp/vault:1.14.1@sha256:436d056e8e2a96c7356720069c29229970466f4f686886289dcc94dfa21d3155
 IMAGE_bind_amd64 := europe-west1-docker.pkg.dev/cert-manager-tests-trusted/cert-manager-infra-images/bind9:9.18-22.04_beta@sha256:8c45ba363b2921950161451cf3ff58dff1816fa46b16fb8fa601d5500cdc2ffc
 IMAGE_sampleexternalissuer_amd64 := ghcr.io/cert-manager/sample-external-issuer/controller:v0.4.0@sha256:964b378fe0dda7fc38ce3f211c3b24c780e44cef13c39d3206de985bad67f294
-IMAGE_projectcontour_amd64 := ghcr.io/projectcontour/contour:v1.29.1@sha256:bb7af851ac5832c315e0863d12ed583cee54c495d58a206f1d0897647505ed70
-# We use the bitnamilegacy image because Bitnami are deprecating support for
-# non-hardened, Debian-based software images in its free tier. See
-# https://github.com/bitnami/containers/issues/83267
-IMAGE_projectcontourenvoy_amd64 := docker.io/bitnamilegacy/envoy:1.29.5-debian-12-r0@sha256:34be30978b7765699c4548a393374a5fea64613352078ec49581be26c2024dec
+IMAGE_kgateway_amd64 := ghcr.io/kgateway-dev/kgateway:v2.1.2@sha256:a6f78f238fb24afce121c4bb8abb8a54bb2d4d0522382601d4e3cc868e9fcce9
 
 IMAGE_ingressnginx_arm64 := registry.k8s.io/ingress-nginx/controller:v1.12.3@sha256:800048a4cdf4ad487a17f56d22ec6be7a34248fc18900d945bc869fee4ccb2f7
 IMAGE_kyverno_arm64 := reg.kyverno.io/kyverno/kyverno:v1.16.2@sha256:b6fa2b1483438ad1faf7a34632d4eee90b477ef58d0bbe7164acbec601d66266
@@ -44,11 +40,7 @@ IMAGE_kyvernopre_arm64 := reg.kyverno.io/kyverno/kyvernopre:v1.16.2@sha256:f48d3
 IMAGE_vault_arm64 := docker.io/hashicorp/vault:1.14.1@sha256:27dd264f3813c71a66792191db5382f0cf9eeaf1ae91770634911facfcfe4837
 IMAGE_bind_arm64 := europe-west1-docker.pkg.dev/cert-manager-tests-trusted/cert-manager-infra-images/bind9:9.18-22.04_beta@sha256:7fcfebdfacf52fa0dee2b1ae37ebe235fe169cbc404974c396937599ca69da6f
 IMAGE_sampleexternalissuer_arm64 := ghcr.io/cert-manager/sample-external-issuer/controller:v0.4.0@sha256:bdff00089ec7581c0d12414ce5ad1c6ccf5b6cacbfb0b0804fefe5043a1cb849
-IMAGE_projectcontour_arm64 := ghcr.io/projectcontour/contour:v1.29.1@sha256:dbfec77951e123bf383a09412a51df218b716aaf3fe7b2778bb2f208ac495dc5
-# We use the bitnamilegacy image because Bitnami are deprecating support for
-# non-hardened, Debian-based software images in its free tier. See
-# https://github.com/bitnami/containers/issues/83267
-IMAGE_projectcontourenvoy_arm64 := docker.io/bitnamilegacy/envoy:1.29.5-debian-12-r0@sha256:0862aad6a034e822ef6cc0e2f2af697ec924d58b8e9acffba48be5b29a9d9776
+IMAGE_kgateway_arm64 := ghcr.io/kgateway-dev/kgateway:v2.1.2@sha256:acdaa7669ac9b1be6be0581d7c2c3d8b294c89ea03a32c1b6201e1f5d73e70af
 
 # We are using @inteon's fork of Pebble, which adds support for signing CSRs with
 # Ed25519 keys:
@@ -107,14 +99,14 @@ kind-exists: $(bin_dir)/scratch/kind-exists
 #  ---------                -------                   --                     ----------------
 #  e2e-setup-bind           DNS-01 tests              SERVICE_IP_PREFIX.16
 #  e2e-setup-ingressnginx   HTTP-01 Ingress tests     SERVICE_IP_PREFIX.15   *.ingress-nginx.db.http01.example.com
-#  e2e-setup-projectcontour HTTP-01 GatewayAPI tests  SERVICE_IP_PREFIX.14   *.gateway.db.http01.example.com
+#  e2e-setup-gwapi-provider HTTP-01 GatewayAPI tests  SERVICE_IP_PREFIX.14   *.gateway.db.http01.example.com
 .PHONY: e2e-setup
 ## Installs cert-manager as well as components required for running the
 ## end-to-end tests. If the kind cluster does not already exist, it will be
 ## created.
 ##
 ## @category Development
-e2e-setup: e2e-setup-gatewayapi e2e-setup-certmanager e2e-setup-vault e2e-setup-bind e2e-setup-sampleexternalissuer e2e-setup-samplewebhook e2e-setup-pebble e2e-setup-ingressnginx e2e-setup-projectcontour
+e2e-setup: e2e-setup-gatewayapi e2e-setup-certmanager e2e-setup-vault e2e-setup-bind e2e-setup-sampleexternalissuer e2e-setup-samplewebhook e2e-setup-pebble e2e-setup-ingressnginx e2e-setup-gwapi-provider
 
 # The function "image-tar" returns the path to the image tarball for a given
 # image name. For example:
@@ -174,7 +166,7 @@ preload-kind-image: $(call image-tar,kind) | $(NEEDS_CTR)
 	$(CTR) inspect $(IMAGE_kind_$(CRI_ARCH)) 2>/dev/null >&2 || $(CTR) load -i $<
 endif
 
-LOAD_TARGETS=load-$(call image-tar,ingressnginx) load-$(call image-tar,kyverno) load-$(call image-tar,kyvernopre) load-$(call image-tar,bind) load-$(call image-tar,projectcontour) load-$(call image-tar,projectcontourenvoy) load-$(call image-tar,sampleexternalissuer) load-$(call local-image-tar,vaultretagged) load-$(call local-image-tar,pebble) load-$(call local-image-tar,samplewebhook) load-$(bin_dir)/containers/cert-manager-controller-linux-$(CRI_ARCH).tar load-$(bin_dir)/containers/cert-manager-acmesolver-linux-$(CRI_ARCH).tar load-$(bin_dir)/containers/cert-manager-cainjector-linux-$(CRI_ARCH).tar load-$(bin_dir)/containers/cert-manager-webhook-linux-$(CRI_ARCH).tar load-$(bin_dir)/containers/cert-manager-startupapicheck-linux-$(CRI_ARCH).tar
+LOAD_TARGETS=load-$(call image-tar,ingressnginx) load-$(call image-tar,kyverno) load-$(call image-tar,kyvernopre) load-$(call image-tar,bind) load-$(call image-tar,kgateway) load-$(call image-tar,sampleexternalissuer) load-$(call local-image-tar,vaultretagged) load-$(call local-image-tar,pebble) load-$(call local-image-tar,samplewebhook) load-$(bin_dir)/containers/cert-manager-controller-linux-$(CRI_ARCH).tar load-$(bin_dir)/containers/cert-manager-acmesolver-linux-$(CRI_ARCH).tar load-$(bin_dir)/containers/cert-manager-cainjector-linux-$(CRI_ARCH).tar load-$(bin_dir)/containers/cert-manager-webhook-linux-$(CRI_ARCH).tar load-$(bin_dir)/containers/cert-manager-startupapicheck-linux-$(CRI_ARCH).tar
 .PHONY: $(LOAD_TARGETS)
 $(LOAD_TARGETS): load-%: % $(bin_dir)/scratch/kind-exists | $(NEEDS_KIND)
 	$(KIND) load image-archive --name=$(shell cat $(bin_dir)/scratch/kind-exists) $*
@@ -200,7 +192,7 @@ $(LOAD_TARGETS): load-%: % $(bin_dir)/scratch/kind-exists | $(NEEDS_KIND)
 #    tag. The rule will fail and the new digest will be printed out.
 # 3. It prevents us accidentally using the wrong digest when we pin the images
 #    in the variables above.
-$(call image-tar,vault) $(call image-tar,kyverno) $(call image-tar,kyvernopre) $(call image-tar,bind) $(call image-tar,projectcontour) $(call image-tar,projectcontourenvoy) $(call image-tar,sampleexternalissuer) $(call image-tar,ingressnginx): $(bin_dir)/downloaded/containers/$(CRI_ARCH)/%.tar: | $(NEEDS_CRANE)
+$(call image-tar,vault) $(call image-tar,kyverno) $(call image-tar,kyvernopre) $(call image-tar,bind) $(call image-tar,kgateway) $(call image-tar,sampleexternalissuer) $(call image-tar,ingressnginx): $(bin_dir)/downloaded/containers/$(CRI_ARCH)/%.tar: | $(NEEDS_CRANE)
 	@$(eval IMAGE=$(subst +,:,$*))
 	@$(eval IMAGE_WITHOUT_DIGEST=$(shell cut -d@ -f1 <<<"$(IMAGE)"))
 	@$(eval DIGEST=$(subst $(IMAGE_WITHOUT_DIGEST)@,,$(IMAGE)))
@@ -487,43 +479,31 @@ e2e-setup-samplewebhook: load-$(call local-image-tar,samplewebhook) e2e-setup-ce
 		--create-namespace \
 		samplewebhook make/config/samplewebhook/chart >/dev/null
 
-.PHONY: e2e-setup-projectcontour
-e2e-setup-projectcontour: $(call image-tar,projectcontour) load-$(call image-tar,projectcontour) $(call image-tar,projectcontourenvoy) load-$(call image-tar,projectcontourenvoy) make/config/projectcontour/gateway.yaml make/config/projectcontour/contour.yaml $(bin_dir)/scratch/kind-exists | $(NEEDS_HELM) $(NEEDS_KUBECTL)
-	@$(eval CONTOUR_TAG=$(shell tar xfO $< manifest.json | jq '.[0].RepoTags[0]' -r | cut -d: -f2))
-	@$(eval ENVOY_TAG=$(shell tar xfO $(call image-tar,projectcontourenvoy) manifest.json | jq '.[0].RepoTags[0]' -r | cut -d: -f2))
-	$(HELM) repo add bitnami --force-update https://charts.bitnami.com/bitnami >/dev/null
-	# Warning: When upgrading the version of this helm chart, bear in mind that the IMAGE_projectcontour_* images above might need to be updated, too.
-	# Each helm chart version in the bitnami repo corresponds to an underlying application version. Check application versions and chart versions with:
-	# $$ helm search repo bitnami -l | grep -E "contour[^-]"
-	#
-	# TODO(wallrj): The free version of the Bitnami contour chart and the
-	# associated images are deprecated. We are using the docker.io/bitnamilegacy
-	# registry as a stop gap measure until we can move to a different chart. See:
-	# https://github.com/bitnami/charts/blob/main/bitnami/contour/README.md#%EF%B8%8F-important-notice-upcoming-changes-to-the-bitnami-catalog
+.PHONY: e2e-setup-gwapi-provider
+e2e-setup-gwapi-provider: $(call image-tar,kgateway) load-$(call image-tar,kgateway) make/config/kgateway/gateway.yaml make/config/kgateway/gwconfig.yaml $(bin_dir)/scratch/kind-exists | $(NEEDS_HELM) $(NEEDS_KUBECTL)
+	@$(eval KGATEWAY_TAG=$(shell tar xfO $< manifest.json | jq '.[0].RepoTags[0]' -r | cut -d: -f2))
+	@$(eval KGATEWAY_HELM_VERSION=v2.1.2)
+	# Warning: When upgrading the version of this helm chart, bear in mind that the IMAGE_kgateway_* images above might need to be updated, too.
 	$(HELM) upgrade \
 		--install \
-		--wait \
-		--version 18.2.4 \
-		--namespace projectcontour \
 		--create-namespace \
-		--set contour.ingressClass.create=false \
-		--set contour.ingressClass.default=false \
-		--set contour.image.registry=ghcr.io \
-		--set contour.image.repository=projectcontour/contour \
-		--set contour.image.tag=$(CONTOUR_TAG) \
-		--set contour.image.pullPolicy=Never \
-		--set contour.service.type=ClusterIP \
-		--set contour.service.externalTrafficPolicy="" \
-		--set envoy.service.type=ClusterIP \
-		--set envoy.service.externalTrafficPolicy="" \
-		--set envoy.service.clusterIP=${SERVICE_IP_PREFIX}.14 \
-		--set envoy.image.registry=docker.io/bitnamilegacy \
-		--set envoy.image.repository=envoy \
-		--set envoy.image.tag=$(ENVOY_TAG) \
-		--set envoy.image.pullPolicy=Never \
-		--set-file configInline=make/config/projectcontour/contour.yaml \
-		projectcontour bitnami/contour >/dev/null
-	$(KUBECTL) apply --server-side -f make/config/projectcontour/gateway.yaml
+		--namespace kgateway-system \
+		--version $(KGATEWAY_HELM_VERSION) \
+		kgateway-crds oci://cr.kgateway.dev/kgateway-dev/charts/kgateway-crds >/dev/null
+	
+	$(HELM) upgrade \
+		--install \
+		--namespace kgateway-system \
+		--version $(KGATEWAY_HELM_VERSION) \
+		--set controller.image.registry=ghcr.io/kgateway-dev \
+		--set controller.image.repository=kgateway \
+		--set controller.image.tag=$(KGATEWAY_TAG) \
+		--set controller.image.pullPolicy=Never \
+		--set controller.extraEnv.KGW_ENABLE_GATEWAY_API_EXPERIMENTAL_FEATURES=true \
+		kgateway oci://cr.kgateway.dev/kgateway-dev/charts/kgateway >/dev/null
+	
+	sed -e 's|__SERVICE_IP__|$(SERVICE_IP_PREFIX).14|g' make/config/kgateway/gwconfig.yaml | $(KUBECTL) apply --server-side -f -
+	$(KUBECTL) apply --server-side -f make/config/kgateway/gateway.yaml
 
 .PHONY: e2e-setup-sampleexternalissuer
 e2e-setup-sampleexternalissuer: load-$(call image-tar,sampleexternalissuer) $(bin_dir)/scratch/kind-exists | $(NEEDS_KUBECTL)
